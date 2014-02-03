b54df3a541716394e8f0b9ed734b11e079e0cdb1
damien.brugne <damien.brugne@eyefinity.com>
2014-02-03 19:49:47 +0100

    Refactor chat page CSS layout. Begin JS logic.

--- web/web/js/chat.js
+++ web/web/js/chat.js
@@ -37,3 +37,69 @@ var upCall = function () {
         }
     });
 }
+
+
+
+var chatScrollDown = function (e) {
+    // certain browsers have a bug such that scrollHeight is too small
+    // when content does not fill the client area of the element
+    console.log(e);
+    var scrollHeight = Math.max(e.scrollHeight, e.clientHeight);
+    e.scrollTop = scrollHeight - e.clientHeight;
+}
+
+/**
+ * DOM manipulation
+ */
+
+var message = { "id" : 1212, "date" : "19:47:39", "username": "yangs_", "text": "oui, mais pas tout le temps." };
+var user = { "id" : 456, "username": "néné", "op": false, "friend": true };
+
+var chatAddMessage = function (message) {
+    /**
+     * Input:
+     *   message = {
+     *     id,
+     *     date,
+     *     username,
+     *     text,
+     *   }
+     *
+     * Output:
+     *    <p><span class="date">[19:47:39]</span> <span class="username">&lt;albou&gt;</span> <span class="text">oui, mais pas tout le temps.</span></p>
+     */
+
+    var htmlCode = "<p data-message-id='"+message.id+"'><span class='date'>["+message.date+"]</span> <span class='username'>&lt;"+message.username+"&gt;</span> <span class='text'>"+message.text+"</span></p>";
+    $("#messages").append(htmlCode);
+
+    $("#messages").each( function() { chatScrollDown(this); });
+}
+
+var chatRemoveMessage = function (messageid) {
+    $('[data-message-id="'+messageid+'"]').remove();
+    $("#messages").each( function() { chatScrollDown(this); });
+}
+
+var chatAddUser = function (user) {
+    /**
+     * Input:
+     *   user = {
+     *     id,
+     *     username,
+     *     op,
+     *     friend,
+     *   }
+     *
+     * Output:
+     *    <li class="op">Pomf</li>
+     */
+    var specialClasses = '';
+    if (user.op == true) specialClasses += ' op';
+    if (user.friend == true) specialClasses += ' friend';
+    var htmlCode = "<li data-user-id='"+user.id+"' class='"+specialClasses+"'>"+user.username+"</li>";
+    $("#users > ul").append(htmlCode);
+}
+
+var chatRemoveUser = function (userid) {
+    $('[data-user-id="'+userid+'"]').remove();
+}
