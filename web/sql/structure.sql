7fbb3b9e39d31de5af3e96dd16ce48c3619a9f86
damien.brugne <damien.brugne@eyefinity.com>
2014-02-03 23:27:22 +0100

    Add SQL structure and sample data files.

---
+++ web/sql/structure.sql
@@ -0,0 +1,67 @@
+--
+-- Structure de la table `users`
+--
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(100) NOT NULL DEFAULT '',
+  `password` VARCHAR(255) NOT NULL DEFAULT '',
+  `email` VARCHAR(255) NOT NULL DEFAULT '',
+  `salt` VARCHAR(255) NOT NULL DEFAULT '',
+  `roles` VARCHAR(255) NOT NULL DEFAULT '',
+  `time_created` INT NOT NULL DEFAULT 0,
+  `first_login` INT NOT NULL DEFAULT 0,
+  `last_login` INT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_username` (`username`),
+  UNIQUE KEY `unique_email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Structure de la table `chat_messages`
+--
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE IF NOT EXISTS `messages` (
+  `id` int(11) NOT NULL auto_increment,
+  `user_id` int(11) NOT NULL,
+  `time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `message` varchar(255) collate latin1_general_ci NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Structure de la table `onlines`
+--
+DROP TABLE IF EXISTS `onlines`;
+CREATE TABLE IF NOT EXISTS `onlines` (
+  `id` int(11) NOT NULL auto_increment,
+  `ip` varchar(100) collate latin1_general_ci NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `status` enum('0','1','2') collate latin1_general_ci NOT NULL,
+  `time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Structure de la table `rooms`
+--
+DROP TABLE IF EXISTS `rooms`;
+CREATE TABLE IF NOT EXISTS `rooms` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(100) collate latin1_general_ci NOT NULL,
+  `topic` varchar(300) collate latin1_general_ci NOT NULL,
+  `protected` tinyint(1) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Structure de la table `tv_channels`
+--
+DROP TABLE IF EXISTS `tv_channels`;
+CREATE TABLE IF NOT EXISTS `tv_channels` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(100) collate latin1_general_ci NOT NULL,
+  `logo` varchar(100) collate latin1_general_ci NOT NULL,
+  `url` varchar(255) collate latin1_general_ci NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
