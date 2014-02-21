779ef867646faa341380fbbeba3bcf90d81dfd19
damien.brugne <damien.brugne@gmail.com>
2014-02-21 14:43:29 +0100

    Remove obsolete classes and server feature (test and log for 9000 port).

--- web/bin/server.php
+++ web/bin/server.php
@@ -16,8 +16,6 @@ use Symfony\Component\HttpFoundation\Session\Storage\Handler\PdoSessionHandler;
 use Server\Components\ChatRoom;
 use Server\Components\Bot;
 use Server\Components\Authentication;
-use Server\Components\PortLogger;
-use Server\Components\NullComponent;
 use Server\Components\MessageLogger;
 
 use Monolog\Logger;
@@ -93,22 +91,12 @@ $logout->pushHandler($stdout);
 // The all mighty event loop
 $loop = Factory::create();
 
-// This little thing is to check for connectivity...
-// As a case study, when people connect on port 80, we're having them
-//  also connect on port 9000 and increment a counter if they connect.
-// Later, we can publish the results and find out if WebSockets over
-//  a port other than 80 is viable (theory is blocked by firewalls).
-//$context = new Context($loop);
-//$push = $context->getSocket(ZMQ::SOCKET_PUSH);
-//$push->connect('tcp://127.0.0.1:5555');
-
 // Setup our Ratchet ChatRoom application
 $webSock = new Reactor($loop);
 $webSock->listen(8080, '0.0.0.0');
 $webServer = new IoServer(           // Basic I/O with clients, aww yeah
     new HttpServer(
         new WsServer(                    // Boom! WebSockets
-//            new PortLogger($push, 8080,    // Compare vs the almost over 9000 conns
                 new MessageLogger(       // Log events in case of "oh noes"
                     new SessionProvider(
                         new Authentication (
@@ -126,7 +114,6 @@ $webServer = new IoServer(           // Basic I/O with clients, aww yeah
                     , $login
                     , $logout
                 )
-//            )
         )
     )
     , $webSock
@@ -138,15 +125,6 @@ $flashSock->listen(843, '0.0.0.0');
 $policy = new FlashPolicy;
 $policy->addAllowedAccess('*', 8080);
 $webServer = new IoServer($policy, $flashSock);
-
-//$logSock = new Reactor($loop);
-//$logSock->listen(9000, '0.0.0.0');
-//$zLogger = new IoServer(
-//    new WsServer(
-//        new PortLogger($push, 9000, new NullComponent)
-//    )
-//    , $logSock
-//);
 
 // GO GO GO!
 $loop->run();
