a80f730c8ef4e71cc7ee2230e7eb1fa584d1c97c
damien.brugne <damien.brugne@gmail.com>
2014-03-05 00:20:40 +0100

    Remove debug code

--- web/web/js/autobahn.js
+++ web/web/js/autobahn.js
@@ -2865,7 +2865,6 @@ var global = this;
                }
 
                self._subscriptions[subid].forEach(function (callback) {
-                    console.log(callback);
                   callback(uri2, val);
                });
             }
