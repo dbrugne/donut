55bd5ee1302194b54bf188d669bf4e0d63802822
damien.brugne <damien.brugne@gmail.com>
2014-04-01 13:24:41 +0200

    Repair #anchor router for room focus (after # introduction in room name).

--- web/web/public/js/App.js
+++ web/web/public/js/App.js
@@ -88,11 +88,11 @@ $(function() {
         },
 
         root: function() {
-            //
+            console.log('router: home');
         },
 
         focusRoom: function(name) {
-            Chat.discussions.focusRoomByName(name);
+            Chat.discussions.focusRoomByName('#'+name);
         },
 
         focusOneToOne: function(username) {
@@ -100,7 +100,7 @@ $(function() {
         },
 
         default: function() {
-            console.log('go in default');
+            console.log('router: default');
             // @todo : handle 404 type behavior in DOM
         }
     });
