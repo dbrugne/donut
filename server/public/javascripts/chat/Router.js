b9a2f8d0bc3d8c8985f7233824d5f1ce02bdc829
damien.brugne <damien.brugne@gmail.com>
2014-04-29 09:17:41 +0200

    Format

---
+++ server/public/javascripts/chat/Router.js
@@ -0,0 +1,81 @@
+define([
+  'underscore',
+  'backbone',
+  'collections/discussions',
+  'models/client',
+  'models/current-user',
+  'views/main'
+], function (_, Backbone, discussions, client, currentUser, mainView) {
+
+    // @todo : now window reopen not pertubate routing:
+    // - should change room focus/open and onetoone focus open to use router only
+    // - should auto join room/onetoone on arriving on page
+    // - should auto focus first room if no is focused
+
+  var ChatRouter = Backbone.Router.extend({
+
+    routes: {
+      '':                 'root',
+      'room/:name':       'focusRoom',
+      'user/:user':       'focusOneToOne',
+      '*default':         'default'
+    },
+
+    initialize: function () {
+      // Server connection is established
+      this.listenTo(client, 'connect', function(data) {
+        if (!window.historyStarted) {
+          Backbone.history.start();
+          window.historyStarted = true;
+        }
+      });
+
+      // Get user identity from server
+      var that = this;
+      this.listenTo(client, 'welcome', function(data) {
+        // Update current user data
+        _.each(Object.keys(data), function(propertyKey) {
+          currentUser.set(propertyKey, data[propertyKey]);
+        });
+        console.log('Hello '+currentUser.get('username')+'!');
+
+        // Join current user rooms
+        _.each(data.rooms, function(room) {
+          client.join(room);
+        });
+      });
+
+      // Everything is in place/bound, connection!
+      client.connect();
+    },
+
+    root: function() {
+//      console.log('router: home');
+      discussions.focus();
+    },
+
+    focusRoom: function(name) {
+//      console.log('router: focusRoom '+name);
+      discussions.focusRoomByName('#'+name);
+    },
+
+    focusOneToOne: function(username) {
+//      console.log('router: focusOneToOne ' + username);
+      discussions.focusOneToOneByUsername(username);
+    },
+
+    default: function() {
+//      console.log('router: default');
+      // @todo : handle 404 type behavior in DOM
+    }
+  });
+
+  var initialize = function () {
+    var router = new ChatRouter();
+  };
+
+  return {
+    initialize: initialize
+  };
+
+});
