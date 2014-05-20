81ae5edab97b35987183102f52012d7c0b7f8193
Ubuntu <ubuntu@ip-172-31-9-69.eu-west-1.compute.internal>
2014-05-20 21:57:38 +0200

    Fix a uncorrect case filename due to Windows FS.

---
+++ server/public/javascripts/chat/router.js
@@ -0,0 +1,52 @@
+define([
+  'underscore',
+  'backbone',
+  'models/client',
+  'views/main'
+], function (_, Backbone, client, mainView) {
+  var ChatRouter = Backbone.Router.extend({
+    routes: {
+      '':                 'root',
+      'room/:name':       'focusRoom',
+      'user/:user':       'focusOneToOne',
+      '*default':         'default'
+    },
+
+    clientOnline: false,
+
+    initialize: function(options) {
+      var that = this;
+      // Watch for client connection state
+      this.listenTo(mainView, 'ready', function() {
+        that.clientOnline = true;
+        Backbone.history.start();
+      });
+      this.listenTo(client, 'disconnect', function() {
+        that.clientOnline = false;
+        Backbone.history.stop();
+      });
+    },
+
+    root: function() {
+      console.log('router: home');
+      mainView.focusHome();
+    },
+
+    focusRoom: function(name) {
+      console.log('router: focusRoom '+name);
+      mainView.focusRoomByName('#'+name);
+    },
+
+    focusOneToOne: function(username) {
+      console.log('router: focusOneToOne ' + username);
+      mainView.focusOneToOneByUsername(username);
+    },
+
+    default: function() {
+      console.log('router: default');
+      // @todo : handle 404 type behavior in DOM
+    }
+  });
+
+  return new ChatRouter();
+});
