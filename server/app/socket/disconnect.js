92f1af9abe13c37f742acba5e3e7fc6d7dd00ce9
dbrugne <damien.brugne@gmail.com>
2014-05-21 08:58:07 +0200

    Repair room:out messages on client disconnection.

--- server/app/socket/disconnect.js
+++ server/app/socket/disconnect.js
@@ -3,6 +3,19 @@ var activityRecorder = require('../activity-recorder');
 
 module.exports = function(io, socket) {
 
+  // Inform other rooms users
+  Object.keys(io.sockets.manager.roomClients[socket.id]).forEach(function(key) {
+    if (key == '') return;
+    if (key.substring(0, 2) != '/#') return;
+
+    var roomName = key.substring(1);
+
+    io.sockets.in(roomName).emit('room:out', {
+      name: roomName,
+      user_id: socket.getUserId()
+    });
+  });
+
   // Multi-devices
   socket.leave('user:'+socket.getUserId());
 
