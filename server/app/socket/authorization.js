ef6253ec80f7ac8282160a664fe1e886e181ace0
dbrugne <damien.brugne@gmail.com>
2014-05-21 22:50:30 +0200

    Add a "stresser" script to generate virtual connections stress on server.

--- server/app/socket/authorization.js
+++ server/app/socket/authorization.js
@@ -1,13 +1,12 @@
 var delegate_error = require('./error');
 var activityRecorder = require('../activity-recorder');
+var User = require('../models/user');
 
 module.exports = {
 
   success: function(data, accept) {
     // could add ACL check here if needed
-
     accept(null, true);
-
     activityRecorder('authorization:success', '', data);
   },
 
@@ -15,6 +14,20 @@ module.exports = {
     if(error) {
       throw new Error(message);
     }
+
+    // Test environment only! (allow virtual client connexion opening)
+    if (process.env.NODE_ENV == 'dev' || process.env.NODE_ENV == 'test') {
+      console.log([data, message, error, accept]);
+      User.findById(data.query.virtualuserid, function(err, virtualUser) {
+        if (err) throw new Error('Error while retrieving virtual user: '+err);
+        if (!virtualUser) throw new Error('No corresponding virtual user found');
+        data['user'] = virtualUser;
+        data['logged_in'] = true;
+        accept(null, true);
+      });
+      return;
+    }
+    // End - Test environment only!
 
     accept(null, false);
 
---
+++ stresser/random.js
@@ -0,0 +1,23 @@
+module.exports = {
+  chars: '0123456789abcdefghiklmnopqrstuvwxyz', // ABCDEFGHIJKLMNOPQRSTUVWXYZ
+  number: function(length) {
+    length = length ? length : 7;
+
+    var number = Math.floor((Math.random() * Math.pow(10,length)) + 1);
+    return number;
+  },
+  string: function(length) {
+    length = length ? length : 32;
+
+    var string = '';
+    for (var i = 0; i < length; i++) {
+      var randomNumber = Math.floor(Math.random() * this.chars.length);
+      string += this.chars.substring(randomNumber, randomNumber + 1);
+    }
+
+    return string;
+  },
+  email: function() {
+    return this.string(10)+'@'+this.string(15)+'.'+this.string(3);
+  }
+};
