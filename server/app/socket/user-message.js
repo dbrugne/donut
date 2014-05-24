48d68ad8ba24a4e105b2c1aa9f5c837799779ad7
dbrugne <damien.brugne@gmail.com>
2014-05-24 13:43:30 +0200

    Repair bug that avoid onetoones message receiver to get message (Model._id is an object and non well toString()'ed by JSON engine)

--- server/app/socket/user-message.js
+++ server/app/socket/user-message.js
@@ -14,7 +14,7 @@ module.exports = function(io, socket, data) {
       return error('Unable to retrieve this user: ' + data.username);
 
     var from = socket.getUserId();
-    var to = userTo._id; // @todo : test that data.to exists in database
+    var to = userTo._id.toString(); // @todo : test that data.to exists in database
 
     var message = {
       from: fromUsername,
@@ -25,10 +25,12 @@ module.exports = function(io, socket, data) {
       username: fromUsername
     };
 
-    io.sockets.in('user:' + from).emit('user:message', message);
-    if (fromUsername !==  toUsername) { // only if sender is not also the receiver
-      io.sockets.in('user:' + to).emit('user:message', message);
-    }
+    // Broadcast message to all 'sender' devices
+    io.sockets.in('user:'+from).emit('user:message', message);
+
+    // (if sender!=receiver) Broadcast message to all 'receiver' devices
+    if (fromUsername !==  toUsername)
+      io.sockets.in('user:'+to).emit('user:message', message);
 
     // Activity
     activityRecorder('user:message', socket.getUserId(), data);
