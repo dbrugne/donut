824ac510731a5fef8908c300796afb1e66fcafdc
dbrugne <damien.brugne@gmail.com>
2014-05-29 22:24:32 +0200

    Minor

--- server/public/javascripts/chat/index.js
+++ server/public/javascripts/chat/index.js
@@ -1,24 +1,26 @@
 require.config({
   paths: {
-      'jquery': '../vendor/jquery/dist/jquery',
-      'bootstrap': '../vendor/bootstrap/dist/js/bootstrap',
-      'text': '../vendor/requirejs-text/text',
-      'socket.io': '/socket.io/socket.io',
-      'underscore': '../vendor/underscore-amd/underscore',
-      'backbone': '../vendor/backbone-amd/backbone',
+      'jquery'                    : '../vendor/jquery/dist/jquery',
+      'bootstrap'                 : '../vendor/bootstrap/dist/js/bootstrap',
+      'text'                      : '../vendor/requirejs-text/text',
+      'socket.io'                 : '/socket.io/socket.io',
+      'underscore'                : '../vendor/underscore-amd/underscore',
+      'backbone'                  : '../vendor/backbone-amd/backbone',
        // for cloudinary upload, not really used but a sucker dev have implement AMD as a bastard
-      'jquery.ui.widget': '../vendor/blueimp-file-upload/js/vendor/jquery.ui.widget',
-      'jquery.iframe-transport': '../vendor/blueimp-file-upload/js/jquery.iframe-transport',
-      'jquery.fileupload': '../vendor/blueimp-file-upload/js/jquery.fileupload',
+      'jquery.ui.widget'          : '../vendor/blueimp-file-upload/js/vendor/jquery.ui.widget',
+      'jquery.iframe-transport'   : '../vendor/blueimp-file-upload/js/jquery.iframe-transport',
+      'jquery.fileupload'         : '../vendor/blueimp-file-upload/js/jquery.fileupload',
        // end of bastard
-      'jquery.cloudinary': '../vendor/cloudinary_js/js/jquery.cloudinary',
-      'jquery.dateformat': '../plugins/jquery.dateformat',
-      'jquery.insertatcaret': '../plugins/jquery.insertatcaret'
+      'jquery.cloudinary'         : '../vendor/cloudinary_js/js/jquery.cloudinary',
+      'jquery.dateformat'         : '../plugins/jquery.dateformat',
+      'jquery.insertatcaret'      : '../plugins/jquery.insertatcaret',
+      'jquery.linkify'            : '../plugins/jquery.linkify.min'
   },
   shim: {
-    'jquery.cloudinary': ['jquery'],
-    'jquery.dateformat': ['jquery'],
-    'jquery.insertatcaret': ['jquery'],
+    'jquery.cloudinary'     : ['jquery'],
+    'jquery.dateformat'     : ['jquery'],
+    'jquery.insertatcaret'  : ['jquery'],
+    'jquery.linkify'        : ['jquery'],
     'bootstrap': {
       deps: ['jquery']
     }
