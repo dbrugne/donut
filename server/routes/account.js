d45fb10a0060235e90ec3b2ce72584996f59f807
dbrugne <damien.brugne@gmail.com>
2014-06-01 18:00:22 +0200

    Add owner room list on account

--- server/routes/account.js
+++ server/routes/account.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
-var isLoggedIn = require('../app/isloggedin');
+var isLoggedIn = require('../app/middlewares/isloggedin');
+var Room = require('../app/models/room');
 
 router.get('/account', isLoggedIn, function(req, res) {
-  res.render('account', { avatarUrl: req.user.avatarUrl('medium') });
+
+  var onSuccess = function(err, rooms) {
+    if (err) {
+      console.log(err);
+      req.flash('error', err)
+      return res.redirect('/');
+    }
+
+    var json = [];
+    for (var i=0; i<rooms.length; i++) {
+      var _json = rooms[i].toJSON();
+      _json.uri = _json.name.replace('#', '');
+      json.push(_json);
+    }
+
+    return res.render('account', {
+      avatarUrl: req.user.avatarUrl('medium'),
+      rooms: json
+    });
+  };
+
+  Room.find({owner: req.user._id.toString()}, onSuccess);
+
 });
 
 module.exports = router;
