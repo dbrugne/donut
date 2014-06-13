0194ae9aa177b6499821437cb8fb22b3d3eac044
dbrugne <damien.brugne@gmail.com>
2014-06-13 11:05:15 +0200

    Fix a GA tracking bug (http://stackoverflow.com/questions/15992734/why-do-i-get-storage-not-available-aborting-hit-with-google-analytics): 'Storage not available. Aborting hit.'

--- server/app/middlewares/googleanalytics.js
+++ server/app/middlewares/googleanalytics.js
@@ -7,7 +7,7 @@ var template = "<script>"
   + "  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)"
   + "  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');"
   + ""
-  + "ga('create', '{{UID}}', '{{FQDN}}');"
+  + "ga('create', '{{UID}}', '{{FQDN}}', {'cookieDomain': 'none'});"
   + "ga('send', 'pageview');"
   + "</script>";
 
