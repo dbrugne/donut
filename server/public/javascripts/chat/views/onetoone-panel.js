c26778cdf0d1ef19333517f9447662b9c0b4bbe1
dbrugne <damien.brugne@gmail.com>
2014-07-03 11:21:15 +0200

    Implement onetoone header as subview.

--- server/public/javascripts/chat/views/onetoone-panel.js
+++ server/public/javascripts/chat/views/onetoone-panel.js
@@ -2,18 +2,21 @@ define([
   'jquery',
   'underscore',
   'backbone',
+  'views/onetoone-header',
   'views/discussion-panel',
   'text!templates/onetoone-panel.html'
-], function ($, _, Backbone, DiscussionPanelView, oneToOnePanelTemplate) {
+], function ($, _, Backbone, OneToOneHeaderView, DiscussionPanelView, oneToOnePanelTemplate) {
   var OneToOnePanelView = DiscussionPanelView.extend({
 
     template: _.template(oneToOnePanelTemplate),
 
     _initialize: function() {
       this.listenTo(this.model, 'change:status', this.updateStatus);
+      this.headerView = new OneToOneHeaderView({el: this.$el.find('.header'), model: this.model});
       this.updateStatus();
     },
     _remove: function(model) {
+      this.headerView.remove();
     },
     _renderData: function() {
       var data = this.model.toJSON();
