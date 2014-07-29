0ff214cfa59df6d277d4b91c611eab32a3c0a2d6
dbrugne <damien.brugne@gmail.com>
2014-07-29 18:54:17 +0200

    Minor

--- server/ws/home.js
+++ server/ws/home.js
@@ -5,7 +5,7 @@ module.exports = function (io, socket) {
   // Home data
   helper.homeData(io, function(homeData) {
     // Online user data
-    helper.onlineData(io, 5, function(usersData) {
+    helper.onlineData(io, 500, function(usersData) {
       homeData.onlines = usersData;
       // Send data
       socket.emit('home', homeData);
