7d7d4dea2714921587cff663de281940d06fa355
dbrugne <damien.brugne@gmail.com>
2014-09-19 09:56:44 +0200

    Minor

--- server/app/hello-dolly.js
+++ server/app/hello-dolly.js
@@ -1,15 +1,15 @@
 var _ = require('underscore');
 
-var lyrics = [
+var quotes = [
   "Ravi de vous revoir, %u",
   "Sympa de vous revoir, %u",
   "Quel plaisir de vous revoir %u",
   "Sympa d'être passé %u",
-  "Je vous sers un petit café %u ?",
-  "Un petit donut %u ?",
-  "Ca va être une belle journée non %u ?",
+  "Un petit café %u ?",
+  "Un p'tit donut %u ?",
+  "Belle journée %u, non ?",
   "Bien le bonjour %u",
-  "Joyeux non-anniversaire %u !",
+//  "Joyeux non-anniversaire %u !",
   "Ouaish ouaish %u",
   "Salutations %u",
   "Ca fait un bail %u",
@@ -17,17 +17,17 @@ var lyrics = [
   "Peace and love, %u",
   "Ahoy-Hoy %u !",
   "Quoi de neuf %u ?",
-  "Super coupe de cheveux %u",
+//  "Super coupe de cheveux %u",
   "Buenos dias %u",
   "Enfin réveillé %u ?",
-  "Vous avez l'air en forme %u",
-  "Vous avez bonne mine %u",
+//  "Vous avez l'air en forme %u",
+//  "Vous avez bonne mine %u",
   "Salut %u, ça va ?",
   "La forme %u ?"
 ];
 
 module.exports = function() {
 
-  return _.sample(lyrics);
+  return _.sample(quotes);
 
 };
