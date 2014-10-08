448a0f4411471610468b6f3fc6d4d2f1f8f3b798
dbrugne <damien.brugne@gmail.com>
2014-10-08 16:02:29 +0200

    Minor

--- server/ws/helper.js
+++ server/ws/helper.js
@@ -4,7 +4,7 @@ var configuration = require('../config/index');
 var User = require('../app/models/user');
 var Room = require('../app/models/room');
 var Activity = require('../app/models/activity');
-var HistoryRoom = require('../app/models/history_room');
+var HistoryRoom = require('../app/models/historyroom');
 var sanitize = require('sanitize-caja');
 var expressValidator = require('../app/validator');
 
