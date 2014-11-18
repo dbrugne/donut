d44c0cceeb2ea762d7fa88ac12f7021c92864e4d
dbrugne <damien.brugne@gmail.com>
2014-11-18 10:21:23 +0100

    Update production configuration file for the new MongoDB server.

--- server/config/config.production.js
+++ server/config/config.production.js
@@ -1,7 +1,7 @@
 var conf = require('./config.global');
 
 conf.fqdn = 'donut.me';
-conf.mongo.url = 'mongodb://localhost:27017/chat';
+conf.mongo.url = 'mongodb://localhost:27017/donut';
 conf.facebook.clientID = '328569463966926';
 conf.facebook.clientSecret = 'e496a32d53acc5d7b855051fd15eb754';
 conf.facebook.callbackURL = 'http://donut.me/login/facebook/callback';
