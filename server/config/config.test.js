68ef3562184743006f11419ad163849fdfbfa759
safe user for node process <donut@nodeTEST.donut.me>
2014-11-24 20:14:22 +0100

    Correct Mongo config for test env

--- server/config/config.test.js
+++ server/config/config.test.js
@@ -2,7 +2,7 @@ var conf = require('./config.global');
 
 conf.fqdn = 'test.donut.me';
 conf.less.force = true;
-conf.mongo.url = 'mongodb://localhost:27017/chat';
+conf.mongo.url = 'mongodb://localhost:27017/donut';
 conf.facebook.callbackURL = 'http://chat.local:3000/login/facebook/callback';
 conf.google.analytics.uid = 'UA-51674523-1';
 
