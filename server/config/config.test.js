d3407e77b259d215b647ca377ba9ebdc7537226d
dbrugne <damien.brugne@gmail.com>
2014-11-25 09:56:26 +0100

    Set Facebook test App for test environement

--- server/config/config.test.js
+++ server/config/config.test.js
@@ -3,7 +3,9 @@ var conf = require('./config.global');
 conf.fqdn = 'test.donut.me';
 conf.less.force = true;
 conf.mongo.url = 'mongodb://localhost:27017/donut';
-conf.facebook.callbackURL = 'http://chat.local:3000/login/facebook/callback';
+conf.facebook.clientID = '400694556754416';
+conf.facebook.clientSecret = '0ae02262f9a318add8b50378eaaf42ce';
+conf.facebook.callbackURL = 'http://test.donut.me/login/facebook/callback';
 conf.google.analytics.uid = 'UA-51674523-1';
 
 module.exports = conf;
