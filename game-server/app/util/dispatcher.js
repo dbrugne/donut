86f7addb9f789f631e0f2bc9e3c437fe05f58c54
dbrugne <damien.brugne@gmail.com>
2014-12-02 23:27:19 +0100

    Minor

--- game-server/app/util/dispatcher.js
+++ game-server/app/util/dispatcher.js
@@ -1,7 +1,9 @@
+var debug = require('debug')('donut:server:dispatch');
 var crc = require('crc');
 
 // select an item from list based on key
 module.exports.dispatch = function(key, list) {
+	debug('dispatcher called with key: '+key);
 	var index = Math.abs(crc.crc32(key)) % list.length;
 	return list[index];
 };
