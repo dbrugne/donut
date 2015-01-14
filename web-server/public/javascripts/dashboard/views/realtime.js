56704a14777110180f855c97b948c1e017e4f6d7
dbrugne <damien.brugne@gmail.com>
2015-01-14 12:23:16 +0100

    On dashboard the connexion with pomelo master is functionnal (but with a 0.8.7 socket.io-client only).  Should implement requesting and displaying of data later: views/realtime.js

--- web-server/public/javascripts/dashboard/views/realtime.js
+++ web-server/public/javascripts/dashboard/views/realtime.js
@@ -2,8 +2,28 @@ define([
   'jquery',
   'underscore',
   'backbone',
+  'models/client',
   'text!templates/realtime.html'
-], function ($, _, Backbone, htmlTemplate) {
+], function ($, _, Backbone, client, htmlTemplate) {
+
+  client.connect('browser-' + Date.now(), 'chat.local', 3015, function(err){ // nginx 3015 => 3005
+    if(err) {
+      console.error('fail to connect to admin console server:');
+      console.error(err);
+    } else {
+      console.info('admin console connected.');
+    }
+  });
+
+  // console usage exemple in /dashboard#realtime
+  //client.request('monitorLog', {number:200,logfile:'pomelo',serverId:'connector-server-1'} , function(err, msg) {
+  //  if(err) {
+  //    console.error('fail to request monitorLog info:');
+  //    console.error(err);
+  //    return;
+  //  }
+  //  console.log(msg);
+  //});
 
   var RealtimeView = Backbone.View.extend({
 
