e2f5a3c577a77d0f90735afb240f3104fe294f6d
dbrugne <damien.brugne@gmail.com>
2015-01-14 12:42:43 +0100

    Minor

--- web-server/public/javascripts/dashboard/views/realtime.js
+++ web-server/public/javascripts/dashboard/views/realtime.js
@@ -6,14 +6,14 @@ define([
   'text!templates/realtime.html'
 ], function ($, _, Backbone, client, htmlTemplate) {
 
-  client.connect('browser-' + Date.now(), 'chat.local', 3015, function(err){ // nginx 3015 => 3005
-    if(err) {
-      console.error('fail to connect to admin console server:');
-      console.error(err);
-    } else {
-      console.info('admin console connected.');
-    }
-  });
+  //client.connect('browser-' + Date.now(), 'chat.local', 3015, function(err){ // nginx 3015 => 3005
+  //  if(err) {
+  //    console.error('fail to connect to admin console server:');
+  //    console.error(err);
+  //  } else {
+  //    console.info('admin console connected.');
+  //  }
+  //});
 
   // console usage exemple in /dashboard#realtime
   //client.request('monitorLog', {number:200,logfile:'pomelo',serverId:'connector-server-1'} , function(err, msg) {
