758f0ee8120d0e87bf99b258af52cdd91e22cd44
dbrugne <damien.brugne@gmail.com>
2015-01-15 16:02:58 +0100

    Restore a logger module that persist donut activity in database

--- shared/models/log.js
+++ shared/models/log.js
@@ -1,12 +1,14 @@
 var mongoose = require('../io/mongoose');
-var debug = require('debug')('donut:server:ws');
+var debug = require('debug')('donut:server:log');
+var logger = require('pomelo-logger').getLogger('donut');
 
 var logSchema = mongoose.Schema({
 
   type            : String,
-  username        : String,
+  uid             : { type: mongoose.Schema.ObjectId, ref: 'User' },
   data            : String,
-  time            : { type: Date, default: Date.now }
+  time            : { type: Date, default: Date.now },
+  duration        : Number
 
 });
 
@@ -14,30 +16,48 @@ logSchema.statics.start = function() {
   return new Date().getTime();
 };
 
-logSchema.statics.log = function(type, username, data, start) {
+/**
+ * Record an activity (a user action on the donut platform) item
+ *
+ * @param type
+ * @param uid
+ * @param data
+ * @param start
+ */
+logSchema.statics.activity = function(type, uid, data, start) {
+  var log = this.record(type, uid, data, start);
+  logger.info(log);
+};
+logSchema.statics.record = function(type, uid, data, start) {
+  // duration
+  var duration;
+  if (start) {
+    duration = (new Date().getTime() - start);
+  }
+
   // prepare model
   var model = new this();
   model.type      = type;
-  model.username  = username;
+  model.uid       = uid;
   model.data      = data;
   model.time      = new Date();
-
-  // log on console
-  var log = "'"+model.username+"' has '"+model.type+"'";
-  if (data)
-    log += " on/with "+data;
-  if (start) {
-    var duration = (new Date().getTime() - start);
-    log += " (in "+duration+"ms)";
-  }
-  debug(log);
+  if (typeof duration != "undefined")
+    model.duration = duration;
 
   // persist
   model.save(function(err) {
     if (err)
-      return debug('Unable to save log record "'+model.type+'" for "'+model.username+'"');
+      return logger.error('Unable to save log record "'+model.type+'" for "'+model.uid+'"');
   });
-};
 
+  // log on console
+  var log = "'"+model.uid+"' has '"+model.type+"'";
+  if (data)
+    log += " on/with "+data;
+  if (start) {
+    log += " (in "+duration+"ms)";
+  }
+  return log;
+};
 
 module.exports = mongoose.model('Log', logSchema);
