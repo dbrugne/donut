fc6fae05f2310af51c9c1dbe66640bc2ddda4ee8
dbrugne <damien.brugne@gmail.com>
2015-01-24 12:27:51 +0100

    Add overriding current page language with fb_locale query param
    #48

--- shared/util/i18next.js
+++ shared/util/i18next.js
@@ -6,7 +6,8 @@ i18next.init({
   resGetPath: require('path').resolve(__dirname, '..', 'locales/__lng__/__ns__.json'),
   resSetPath: require('path').resolve(__dirname, '..', 'locales/__lng__/__ns__.json'),
   cookieName: conf.i18n.cookie,
-  saveMissing: true
+  saveMissing: true,
+  detectLngQS: 'fb_locale' // allow FB open graph crawler to force language (https://developers.facebook.com/docs/opengraph/guides/internationalization?locale=fr_FR)
 });
 
 // hotfix for Express 4.0 compliance, dynamicHelper no longer exist since 3.0
