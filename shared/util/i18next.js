8c48be1f0e41b02d7314cb3fc0a659e01d77600e
dbrugne <damien.brugne@gmail.com>
2015-01-24 18:08:18 +0100

    Make french as default language
    #48

--- shared/util/i18next.js
+++ shared/util/i18next.js
@@ -7,7 +7,9 @@ i18next.init({
   resSetPath: require('path').resolve(__dirname, '..', 'locales/__lng__/__ns__.json'),
   cookieName: conf.i18n.cookie,
   saveMissing: true,
-  detectLngQS: 'force_locale' // set by facebooklocale.js middleware
+  detectLngQS: 'force_locale', // set by facebooklocale.js middleware,
+  fallbackLng: ['fr'],
+  detectLngFromHeaders: false // Chrome set header to prefer english disregarding OS/user language
 });
 
 // hotfix for Express 4.0 compliance, dynamicHelper no longer exist since 3.0
