29e758d8b116072c1982dfe3d21caca471682c72
dbrugne <dbrugne.brugne@gmail.com>
2015-02-02 13:12:29 +0100

    Adjust cookie name for production due to HTTP only+subdomain donut policy (oups!)

--- shared/config/config.production.js
+++ shared/config/config.production.js
@@ -6,5 +6,6 @@ conf.facebook.clientID = '328569463966926';
 conf.facebook.clientSecret = 'e496a32d53acc5d7b855051fd15eb754';
 conf.facebook.callbackURL = 'http://donut.me/login/facebook/callback';
 conf.google.analytics.uid = 'UA-51674523-2';
+conf.sessions.key = 'donut-pomelo.sid';
 
 module.exports = conf;
