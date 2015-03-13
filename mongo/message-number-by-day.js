3dfff6c0aa235930928f8800c7037dcae70b1f33
dbrugne <damien.brugne@gmail.com>
2015-03-13 16:20:52 +0100

    Minor

---
+++ mongo/message-number-by-day.js
@@ -0,0 +1,9 @@
+db['history-room'].aggregate([
+  { $match: { event: 'room:message' } },
+  {
+    $group : {
+      _id : { month: { $month: "$time" }, day: { $dayOfMonth: "$time" } },
+      count: { $sum: 1 }
+    }
+  }
+])
