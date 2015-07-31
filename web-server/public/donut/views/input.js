90c0a7ffb245fe3a8a5bfd411cb671218e7d001d
Yann <y.lastapis@gmail.com>
2015-07-31 10:37:44 +0200

    #291 WIP

--- web-server/public/donut/views/input.js
+++ web-server/public/donut/views/input.js
@@ -20,21 +20,34 @@ define([
 
     images: '',
 
-    KEY : { BACKSPACE : 8, TAB : 9, RETURN : 13, ESC : 27, LEFT : 37, UP : 38, RIGHT : 39, DOWN : 40, COMMA : 188, SPACE : 32, HOME : 36, END : 35 },
+    KEY: {
+      BACKSPACE: 8,
+      TAB: 9,
+      RETURN: 13,
+      ESC: 27,
+      LEFT: 37,
+      UP: 38,
+      RIGHT: 39,
+      DOWN: 40,
+      COMMA: 188,
+      SPACE: 32,
+      HOME: 36,
+      END: 35
+    },
 
     events: {
       //'input .editable'               : 'onInput',
-      'keyup .editable'               : 'onKeyUp',
-      'keydown .editable'             : 'onKeyDown',
-      'click .send'                   : 'sendMessage',
-      'click .add-image'              : 'onAddImage',
-      'click .remove-image'           : 'onRemoveImage',
-      'click .add-smiley'             : 'onOpenSmiley',
-      'click .smileys .smilify'       : 'onPickSmiley',
-      'hover .rollup-container li'    : 'onRollupHover'
+      'keyup .editable': 'onKeyUp',
+      'keydown .editable': 'onKeyDown',
+      'click .send': 'sendMessage',
+      'click .add-image': 'onAddImage',
+      'click .remove-image': 'onRemoveImage',
+      'click .add-smiley': 'onOpenSmiley',
+      'click .smileys .smilify': 'onPickSmiley',
+      'hover .rollup-container li': 'onRollupHover'
     },
 
-    initialize: function(options) {
+    initialize: function (options) {
       this.listenTo(currentUser, 'change:avatar', this.onAvatar);
       this.listenTo(this.model, 'inputActive', this.onActiveChange);
 
@@ -43,12 +56,11 @@ define([
       this.render();
     },
 
-    _remove: function() {
-      //this.$editable.mentionsInput('reset');
+    _remove: function () {
       this.remove();
     },
 
-    render: function() {
+    render: function () {
       this.$el.html(this.template({
         avatar: $.cd.userAvatar(currentUser.get('avatar'), 80),
         bannedMessage: $.t('chat.actions.bannedMessage.__type__'.replace('__type__', this.model.get('type')))
@@ -58,35 +70,6 @@ define([
       this.$preview = this.$el.find('.preview');
       this.$rollUpCtn = this.$el.find('.rollup-container');
 
-      var that = this;
-
-      //// mentions initialisation
-      //this.$editable.mentionsInput({
-      //  minChars: 1,
-      //  elastic: false,
-      //  allowRepeat: true,
-      //  onDataRequest: function (mode, query, callback) {
-      //    if (that.model.get('type') != 'room')
-      //      return [];
-      //    // filter user list
-      //    var data = that.model.users.filter(function(item) {
-      //      return item.get('username').toLowerCase().indexOf(query.toLowerCase()) > -1;
-      //    });
-      //    // decorate user list
-      //    data = _.map(data, function(model, key, list) {
-      //      var avatar = $.cd.userAvatar(model.get('avatar'), 10);
-      //      return {
-      //        id      : model.get('id'),
-      //        name    : model.get('username'),
-      //        avatar  : avatar,
-      //        type    : 'user'
-      //      };
-      //    });
-      //
-      //    callback.call(this, data);
-      //  }
-      //});
-
       if (this.model.isInputActive() === false) { // deactivate input
         this.$el.addClass('inactive');
       } else {
@@ -94,7 +77,7 @@ define([
       }
     },
 
-    onActiveChange: function() {
+    onActiveChange: function () {
       if (this.model.isInputActive() === false) { // deactivate input
         this.$el.addClass('inactive');
       } else {
@@ -108,7 +91,7 @@ define([
     //    .css('bottom', this.$editable.height()+10+'px');
     //},
 
-    onAvatar: function(model, value, options) {
+    onAvatar: function (model, value, options) {
       this.$el.find('.avatar').prop('src', $.cd.userAvatar(value, 80));
     },
 
@@ -118,66 +101,74 @@ define([
      *
      * @param event
      */
-    onKeyDown: function(event) {
+    onKeyDown: function (event) {
       var data = this._getKeyCode();
+      var message = this.$editable.val();
 
+      // Avoid loosing focus when tab is pushed
       if (data.key == this.KEY.TAB)
         event.preventDefault();
+
+      // Avoid sending empty messages
+      if (data.key == this.KEY.RETURN && !data.isShift && message.length == 0) {
+        event.preventDefault();
+        return this.$editable.val('');
+      }
     },
 
-    onKeyUp: function(event) {
+    onKeyUp: function (event) {
       if (event.type != 'keyup')
         return;
 
       var data = this._getKeyCode();
+      var message = this.$editable.val();
 
       // Rollup Closed
       if (this.$rollUpCtn.html().length == 0) {
-
-        if(data.key == this.KEY.RETURN && !data.isShift)
+        if (data.key == this.KEY.RETURN && !data.isShift && message.length != 0)
           return this.sendMessage();
 
         if (data.key == this.KEY.UP && ($(event.currentTarget).val() === ''))
           return this.trigger('editPreviousInput');
 
-        if (!this._isRollupCallValid(event.target.value))
+        if (!this._isRollupCallValid(message))
           return this.onRollUpClose();
 
-        return this.onRollUpCall(event.target.value);
+        return this.onRollUpCall(message);
 
-      // Rollup Open
+        // Rollup Open
       } else {
         // Cleaned the input
         // On key up, if input is empty or push Esc
-        if (event.target.value.length == 0 || data.key == this.KEY.ESC)
+        if (message.length == 0 || data.key == this.KEY.ESC)
           return this.onRollUpClose();
 
-        if(data.key == this.KEY.RETURN && !data.isShift)
+        if (data.key == this.KEY.RETURN && !data.isShift && message.length != 0)
           return this.onRollUpClose(event.target);
 
         if (data.key == this.KEY.UP || data.key == this.KEY.DOWN || data.key == this.KEY.TAB)
           return this._rollupNavigate(data.key, event.target);
 
-        if (!this._isRollupCallValid(event.target.value))
+        if (!this._isRollupCallValid(message))
           return this.onRollUpClose();
 
-        return this.onRollUpCall(event.target.value);
+        return this.onRollUpCall(message);
       }
     },
 
-    _isRollupCallValid: function(val) {
+    _isRollupCallValid: function (val) {
       // get last typed parameter, separated by spaces
       var last = _.last(val.split(' '));
       if (last.length == 0)
         return false;
 
-      if (_.indexOf(['#', '@', '/'], last.substr(0,1)) == -1)
+      if (_.indexOf(['#', '@', '/'], last.substr(0, 1)) == -1)
         return false;
 
       return this.onRollUpCall(last);
     },
 
-    _rollupNavigate: function(key, target) {
+    _rollupNavigate: function (key, target) {
       var currentLi = this.$rollUpCtn.find('li.active');
       var li = '';
       if (key == this.KEY.UP) {
@@ -194,11 +185,11 @@ define([
       if (li.length != 0) {
         currentLi.removeClass('active');
         li.addClass('active');
-        target.value = li.find('.cmdname').html()+' ';
+        target.value = li.find('.cmdname').html() + ' ';
       }
     },
 
-    _getKeyCode: function() {
+    _getKeyCode: function () {
       if (window.event) {
         return {
           key: window.event.keyCode,
@@ -212,10 +203,10 @@ define([
       }
     },
 
-    sendMessage: function(event) {
+    sendMessage: function (event) {
       // Get the message
       var that = this;
-      var message = this.$editable.val().substr(0,this.$editable.val().length -1); // Remove last return caracter
+      var message = this.$editable.val().substr(0, this.$editable.val().length - 1); // Remove last return caracter
 
       var imagesCount = _.keys(that.images).length;
       if (message == '' && imagesCount < 1) // empty message and no image
@@ -224,13 +215,13 @@ define([
       // Mentions, try to find missed ones
       if (that.model.get('type') == 'room') {
         var potentialMentions = message.match(/@([-a-z0-9\._|^]{3,15})/ig); // @todo from constant
-        _.each(potentialMentions, function(p) {
+        _.each(potentialMentions, function (p) {
           var u = p.replace(/^@/, '');
           var m = that.model.users.iwhere('username', u);
           if (m) {
             message = message.replace(
-              new RegExp('@'+u, 'g'),
-              '@['+ m.get('username')+'](user:'+m.get('id')+')'
+              new RegExp('@' + u, 'g'),
+              '@[' + m.get('username') + '](user:' + m.get('id') + ')'
             );
           }
         });
@@ -246,7 +237,7 @@ define([
       // add images
       var images = [];
       if (imagesCount > 0) {
-        _.each(that.images, function(i) {
+        _.each(that.images, function (i) {
           images.push({
             public_id: i.public_id,
             version: i.version,
@@ -261,7 +252,6 @@ define([
 
       // Empty field
       that.$editable.val('');
-      that.$editable.mentionsInput('reset');
 
       // reset images
       that.images = {};
@@ -272,7 +262,7 @@ define([
       return false;
     },
 
-    onAddImage: function(event) {
+    onAddImage: function (event) {
       event.preventDefault();
 
       // @doc: http://cloudinary.com/documentation/upload_widget#setup
@@ -280,14 +270,14 @@ define([
         upload_preset: 'discussion',
         sources: ['local'], // ['local', 'url', 'camera']
         multiple: true,
-        client_allowed_formats: ["png","gif", "jpeg"],
+        client_allowed_formats: ["png", "gif", "jpeg"],
         max_file_size: 20000000, // 20Mo
         max_files: 5,
-        thumbnail_transformation: { width: 80, height: 80, crop: 'fill' }
+        thumbnail_transformation: {width: 80, height: 80, crop: 'fill'}
       };
 
       var that = this;
-      cloudinary.openUploadWidget(options, function(err, result) {
+      cloudinary.openUploadWidget(options, function (err, result) {
           if (err) {
             if (err.message && err.message == 'User closed widget')
               return;
@@ -296,7 +286,7 @@ define([
           if (!result)
             return debug('cloudinary result is empty!');
 
-          _.each(result, function(uploaded) {
+          _.each(result, function (uploaded) {
             // render preview
             that.$preview.find('.add-image').before(that.imageTemplate({data: uploaded}));
             // add to collection
@@ -307,28 +297,28 @@ define([
         }
       );
     },
-    onRemoveImage: function(event) {
+    onRemoveImage: function (event) {
       event.preventDefault();
       var cid = $(event.currentTarget).closest('.image').data('cloudinaryId');
       // remove from collection
       if (this.images[cid])
         delete this.images[cid];
       // remove preview
-      this.$preview.find('.image[data-cloudinary-id="'+cid+'"]').remove();
+      this.$preview.find('.image[data-cloudinary-id="' + cid + '"]').remove();
       // hide previews
       if (_.keys(this.images).length < 1)
         this.hidePreview();
     },
-    showPreview: function() {
+    showPreview: function () {
       this.$preview.show();
       this.trigger('resize');
     },
-    hidePreview: function() {
+    hidePreview: function () {
       this.$preview.hide();
       this.trigger('resize');
     },
 
-    onOpenSmiley: function(event) {
+    onOpenSmiley: function (event) {
       event.preventDefault();
 
       if (!this.$smileyButton) {
@@ -346,7 +336,7 @@ define([
       }
     },
 
-    onPickSmiley: function(event) {
+    onPickSmiley: function (event) {
       event.preventDefault();
 
       var symbol = $.smilifyGetSymbolFromCode($(event.currentTarget).data('smilifyCode'));
@@ -359,21 +349,39 @@ define([
      * @param str, cannot be null here
      * @todo store results in view, to avoid multiple call to client ?
      */
-    onRollUpCall: function(str) {
+    onRollUpCall: function (str) {
       var that = this;
+
+      //if ('@' == str.substr(0,1))
+
+      //// filter user list
+      //  var data = that.model.users.filter(function(item) {
+      //    return item.get('username').toLowerCase().indexOf(query.toLowerCase()) > -1;
+      //  });
+      //// decorate user list
+      //data = _.map(data, function(model, key, list) {
+      //  var avatar = $.cd.userAvatar(model.get('avatar'), 10);
+      //  return {
+      //    id      : model.get('id'),
+      //    name    : model.get('username'),
+      //    avatar  : avatar,
+      //    type    : 'user'
+      //  };
+      //});
+
       client.userRollUp(str, function (data) {
         that.$rollUpCtn.html(that.rollupTemplate(data));
       });
     },
 
-    onRollUpClose: function(target) {
+    onRollUpClose: function (target) {
       if (target)
-        target.value =  this.$rollUpCtn.find('li.active .value').html()+' ';
+        target.value = this.$rollUpCtn.find('li.active .value').html() + ' ';
 
       this.$rollUpCtn.html('');
     },
 
-    onRollupHover: function() {
+    onRollupHover: function () {
       console.log('hover');
     }
 
