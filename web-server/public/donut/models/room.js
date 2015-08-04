0a882dcba9b4967a82218c274b0dc39f0b463f36
dbrugne <damien.brugne@gmail.com>
2015-08-04 15:52:45 +0200

    Minor cleanup
    #267

--- web-server/public/donut/models/room.js
+++ web-server/public/donut/models/room.js
@@ -13,6 +13,7 @@ define([
       return {
         name          : '',
         op            : [],
+        devoices      : [],
         topic         : '',
         avatar        : '',
         poster        : '',
@@ -196,8 +197,6 @@ define([
       this.trigger('freshEvent', model);
     },
     onVoice: function(data) {
-
-      // user.get('is_devoice')
       var user = this.users.get(data.user_id);
       if (user)
         user.set({is_devoice: false});
@@ -207,8 +206,6 @@ define([
       this.trigger('inputActive', data.user_id);
     },
     onDevoice: function(data) {
-
-      // user.get('is_devoice')
       var user = this.users.get(data.user_id);
       if (user)
         user.set({is_devoice: true});
@@ -284,9 +281,7 @@ define([
       return !!(this.get('newmessage') || this.get('newmention') || this.get('newuser'));
     },
     isInputActive: function(userId) {
-      if (this.userIsDevoiced(userId))
-        return false;
-      return true;
+      return !this.userIsDevoiced(userId);
     }
 
   });
