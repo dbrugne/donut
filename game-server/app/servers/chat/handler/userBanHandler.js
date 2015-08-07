e5f82069a4bb2e19a686f2bdf9f31d69c6400034
dbrugne <damien.brugne@gmail.com>
2015-08-07 17:08:01 +0200

    Migrate user:ban/deban under parameters filter
    #325

--- game-server/app/servers/chat/handler/userBanHandler.js
+++ game-server/app/servers/chat/handler/userBanHandler.js
@@ -1,10 +1,7 @@
 var logger = require('../../../../pomelo-logger').getLogger('donut', __filename);
 var async = require('async');
 var _ = require('underscore');
-var Room = require('../../../../../shared/models/room');
-var User = require('../../../../../shared/models/user');
 var oneEmitter = require('../../../util/oneEmitter');
-var inputUtil = require('../../../util/input');
 
 module.exports = function(app) {
 	return new Handler(app);
@@ -26,87 +23,54 @@ var handler = Handler.prototype;
  */
 handler.ban = function(data, session, next) {
 
+  var user = session.__currentUser__;
+  var bannedUser = session.__user__;
+
 	var that = this;
 
 	async.waterfall([
 
 		function check(callback) {
-			if (!data.user_id)
-				return callback('user:ban require uid param');
+			if (!data.username)
+				return callback('require username param');
+
+      if (!user)
+        return callback('unable to retrieve user: ' + session.uid);
+
+      if (!bannedUser)
+        return callback('unable to retrieve bannedUser: ' + data.username);
+
+      if (user.isBanned(bannedUser.id))
+        return callback('this user ' + bannedUser.username + ' is already banned');
 
 			return callback(null);
 		},
 
-		function retrieveUser(callback) {
-			User.findByUid(session.uid).exec(function (err, user) {
-				if (err)
-					return callback('Error while retrieving user '+session.uid+' in user:ban: '+err);
-
-				if (!user)
-					return callback('Unable to retrieve user in user:ban: '+session.uid);
-
-				return callback(null, user);
-			});
-		},
-
-		function retrieveBannedUser(user, callback) {
-			User.findByUid(data.user_id).exec(function (err, bannedUser) {
-				if (err)
-					return callback('Error while retrieving bannedUser '+data.user_id+' in user:ban: '+err);
-
-				if (!bannedUser)
-					return callback('Unable to retrieve bannedUser in user:ban: '+data.user_id);
-
-				return callback(null, user, bannedUser);
-			});
-		},
-
-		function checkIfAlreadyBanned(user, bannedUser, callback) {
-			if (user.isBanned(bannedUser.id))
-				return callback('This user '+bannedUser.username+' is already banned');
-
-			return callback(null, user, bannedUser);
-		},
-
-		function persistOnUser(user, bannedUser, callback) {
+		function persist(callback) {
 			var ban = {
 				user: bannedUser._id,
 				banned_at: new Date()
 			};
-
 			user.update({$addToSet: { bans: ban }}, function(err) {
-				if (err)
-					return callback('Unable to persist ban of '+bannedUser.username);
-
-				return callback(null, user, bannedUser);
+				return callback(err);
 			});
 		},
 
-		function prepareEvent(user, bannedUser, callback) {
-			var event = {
-        by_user_id  : user._id.toString(),
+		function historizeAndEmit(callback) {
+      var event = {
+        by_user_id  : user.id,
         by_username : user.username,
         by_avatar   : user._avatar(),
-        user_id     : bannedUser._id.toString(),
+        user_id     : bannedUser.id,
         username    : bannedUser.username,
         avatar      : bannedUser._avatar()
       };
-
-			return callback(null, user, bannedUser, event);
-		},
-
-		function historizeAndEmit(user, bannedUser, event, callback) {
-			oneEmitter(that.app, {from: user._id, to: bannedUser._id}, 'user:ban', event, function(err, event) {
-				if (err)
-					return callback(err);
-
-				return callback(null, user, bannedUser, event);
-			});
+			oneEmitter(that.app, { from: user._id, to: bannedUser._id }, 'user:ban', event, callback);
 		}
 
 	], function(err) {
 		if (err) {
-			logger.error(err);
+			logger.error('[user:ban] ' + err);
 			return next(null, {code: 500, err: err});
 		}
 
--- game-server/app/servers/chat/handler/userDebanHandler.js
+++ game-server/app/servers/chat/handler/userDebanHandler.js
@@ -1,10 +1,7 @@
 var logger = require('../../../../pomelo-logger').getLogger('donut', __filename);
 var async = require('async');
 var _ = require('underscore');
-var Room = require('../../../../../shared/models/room');
-var User = require('../../../../../shared/models/user');
 var oneEmitter = require('../../../util/oneEmitter');
-var inputUtil = require('../../../util/input');
 
 module.exports = function(app) {
 	return new Handler(app);
@@ -26,89 +23,55 @@ var handler = Handler.prototype;
  */
 handler.ban = function(data, session, next) {
 
+	var user = session.__currentUser__;
+	var bannedUser = session.__user__;
+
 	var that = this;
 
 	async.waterfall([
 
 		function check(callback) {
-			if (!data.user_id)
-				return callback('user:deban require uid param');
+			if (!data.username)
+				return callback('require username param');
+
+      if (!user)
+        return callback('unable to retrieve user: ' + session.uid);
+
+      if (!bannedUser)
+        return callback('unable to retrieve bannedUser: ' + data.username);
+
+      if (!user.isBanned(bannedUser.id))
+        return callback('this user ' + bannedUser.username + ' is not banned');
 
 			return callback(null);
 		},
 
-		function retrieveUser(callback) {
-			User.findByUid(session.uid).exec(function (err, user) {
-				if (err)
-					return callback('Error while retrieving user '+session.uid+' in user:deban: '+err);
-
-				if (!user)
-					return callback('Unable to retrieve user in user:deban: '+session.uid);
-
-				return callback(null, user);
-			});
-		},
-
-		function retrieveUnbannedUser(user, callback) {
-			User.findByUid(data.user_id).exec(function (err, unbannedUser) {
-				if (err)
-					return callback('Error while retrieving unbannedUser '+data.user_id+' in user:deban: '+err);
-
-				if (!unbannedUser)
-					return callback('Unable to retrieve unbannedUser in user:deban: '+data.user_id);
-
-				return callback(null, user, unbannedUser);
-			});
-		},
-
-		function checkIfAlreadyUnbanned(user, unbannedUser, callback) {
-			if (!user.isBanned(unbannedUser.id))
-				return callback('This user '+unbannedUser.username+' is not banned');
-
-			return callback(null, user, unbannedUser);
-		},
-
-		function persistOnUser(user, unbannedUser, callback) {
+		function persist(callback) {
 			var subDocument = _.find(user.bans, function(ban) {
-				if (ban.user.toString() == unbannedUser._id.toString())
+				if (ban.user.toString() == bannedUser.id)
 					return true;
 			});
-
 			user.bans.id(subDocument._id).remove();
-
 			user.save(function(err) {
-				if (err)
-					return callback('Unable to persist deban of '+unbannedUser.username+' on '+user.username);
-
-				return callback(null, user, unbannedUser);
+				return callback(err);
 			});
 		},
 
-		function prepareEvent(user, unbannedUser, callback) {
-			var event = {
-        by_user_id  : user._id.toString(),
+		function historizeAndEmit(callback) {
+      var event = {
+        by_user_id  : user.id,
         by_username : user.username,
         by_avatar   : user._avatar(),
-        user_id     : unbannedUser._id.toString(),
-        username    : unbannedUser.username,
-        avatar      : unbannedUser._avatar()
-			};
-
-			return callback(null, user, unbannedUser, event);
-		},
-
-		function historizeAndEmit(user, unbannedUser, event, callback) {
-			oneEmitter(that.app, {from: user.id, to: unbannedUser.id}, 'user:deban', event, function(err, event) {
-				if (err)
-					return callback(err);
-
-				return callback(null, user, unbannedUser, event);
-			});
+        user_id     : bannedUser.id,
+        username    : bannedUser.username,
+        avatar      : bannedUser._avatar()
+      };
+			oneEmitter(that.app, { from: user._id, to: bannedUser._id }, 'user:deban', event, callback);
 		}
 
 	], function(err) {
 		if (err) {
-			logger.error(err);
+			logger.error('[user:deban] ' + err);
 			return next(null, {code: 500, err: err});
 		}
 
