268c277229abf3510eaf61ca8ae031353193ebe0
dbrugne <damien.brugne@gmail.com>
2015-08-07 13:34:43 +0200

    Migrate room:kick
    #325

--- game-server/app/servers/chat/handler/roomKickHandler.js
+++ game-server/app/servers/chat/handler/roomKickHandler.js
@@ -26,84 +26,59 @@ var handler = Handler.prototype;
  */
 handler.kick = function(data, session, next) {
 
+	var user = session.__currentUser__;
+	var kickedUser = session.__user__;
+	var room = session.__room__;
+
 	var that = this;
 
 	async.waterfall([
 
 		function check(callback) {
 			if (!data.name)
-				return callback('room:kick require room name param');
+				return callback('require room name param');
 
 			if (!data.username)
-				return callback('room:kick require username param');
+				return callback('require username param');
+
+			if (!room)
+				return callback('unable to retrieve room: '+data.name);
+
+			if (!room.isOwnerOrOp(session.uid) && session.settings.admin !== true)
+				return callback('this user '+session.uid+' isn\'t able to kick another user in this room: '+data.name);
+
+			if (!user)
+				return callback('unable to retrieve user: '+session.uid);
+
+      if (!kickedUser)
+        return callback('unable to retrieve kickedUser: '+session.uid);
+
+      if (room.owner.toString() == kickedUser.id)
+        return callback('user '+kickedUser.username+' is owner of '+room.name);
+
+      if (room.users.indexOf(kickedUser.id) === -1)
+        return callback('can\'t kick user '+user.username+' that is not actually in the room: '+data.name);
 
 			return callback(null);
 		},
 
-		function retrieveRoom(callback) {
-			Room.findByName(data.name).exec(function (err, room) {
-				if (err)
-					return callback('Error while retrieving room in room:kick: '+err);
-
-				if (!room)
-					return callback('Unable to retrieve room in room:kick: '+data.name);
-
-				if (!room.isOwnerOrOp(session.uid) && session.settings.admin !== true)
-					return callback('This user '+session.uid+' isn\'t able to kick another user in this room: '+data.name);
-
-				return callback(null, room);
-			});
-		},
-
-		function retrieveUser(room, callback) {
-			User.findByUid(session.uid).exec(function (err, user) {
-				if (err)
-					return callback('Error while retrieving user '+session.uid+' in room:kick: '+err);
-
-				if (!user)
-					return callback('Unable to retrieve user in room:kick: '+session.uid);
-
-				return callback(null, room, user);
-			});
-		},
-
-		function retrieveKickedUser(room, user, callback) {
-			User.findByUsername(data.username).exec(function (err, kickedUser) {
-				if (err)
-					return callback('Error while retrieving kickedUser '+session.uid+' in room:kick: '+err);
-
-				if (!kickedUser)
-					return callback('Unable to retrieve kickedUser in room:kick: '+session.uid);
-
-				// Is the targeted user is the owner
-				if (room.owner.toString() == kickedUser._id.toString())
-					return callback('User '+kickedUser.username+' is owner of '+room.name);
-
-				// Targeted user should be in room
-				if (room.users.indexOf(kickedUser._id.toString()) === -1)
-					return callback('Can\'t kick user '+user.username+' that is not actually in the room: '+data.name);
-
-				return callback(null, room, user, kickedUser);
-			});
-		},
-
-		function persistOnRoom(room, user, kickedUser, callback) {
+		function persistOnRoom(callback) {
 			room.update({$pull: { users: kickedUser._id }}, function(err) {
 				if (err)
-					return callback('Unable to persist kick of '+kickedUser._id.toString()+' on '+room.name);
+					return callback('unable to persist kick of ' + kickedUser.id + ' on '+room.name);
 
-				return callback(null, room, user, kickedUser);
+				return callback(null);
 			});
 		},
 
-		function prepareEvent(room, user, kickedUser, callback) {
+		function prepareEvent(callback) {
 			var event = {
 				name			 : room.name,
 				id				 : room.id,
-				by_user_id : user._id.toString(),
+				by_user_id : user.id,
 				by_username: user.username,
 				by_avatar  : user._avatar(),
-				user_id: kickedUser._id.toString(),
+				user_id: kickedUser.id,
 				username: kickedUser.username,
 				avatar: kickedUser._avatar()
 			};
@@ -111,35 +86,35 @@ handler.kick = function(data, session, next) {
 			if (data.reason)
 				event.reason = data.reason;
 
-			return callback(null, room, user, kickedUser, event);
+			return callback(null, event);
 		},
 
-		function historizeAndEmit(room, user, kickedUser, event, callback) {
+		function historizeAndEmit(event, callback) {
 			roomEmitter(that.app, 'room:kick', event, function(err, sentEvent) {
 				if (err)
-					return callback('Error while emitting room:kick in '+room.name+': '+err);
+					return callback('error while emitting in ' + room.name + ': '+err);
 
-				return callback(null, room, user, kickedUser, sentEvent);
+				return callback(null, sentEvent);
 			});
 		},
 
 		/**
 		 * /!\ .unsubscribeClients come after .historizeAndEmit to allow kicked user to receive message
 		 */
-		function unsubscribeClients(room, user, kickedUser, sentEvent, callback) {
+		function unsubscribeClients(sentEvent, callback) {
 			// search for all the user sessions (any frontends)
-			that.app.statusService.getSidsByUid(kickedUser._id.toString(), function(err, sids) {
+			that.app.statusService.getSidsByUid(kickedUser.id, function(err, sids) {
 				if (err)
 					return callback('Error while retrieving user status: '+err);
 
 				if (!sids || sids.length < 1) {
-					return callback(null, room, user, kickedUser, sentEvent); // the targeted user could be offline at this time
+					return callback(null, sentEvent); // the targeted user could be offline at this time
 				}
 
 				var parallels = [];
 				_.each(sids, function(sid) {
 					parallels.push(function(fn) {
-						that.app.globalChannelService.leave(room.name, kickedUser._id.toString(), sid, function(err) {
+						that.app.globalChannelService.leave(room.name, kickedUser.id, sid, function(err) {
 							if (err)
 								return fn(sid+': '+err);
 
@@ -149,20 +124,20 @@ handler.kick = function(data, session, next) {
 				});
 				async.parallel(parallels, function(err, results) {
 					if (err)
-						return callback('Error while unsubscribing user '+kickedUser._id.toString()+' from '+room.name+': '+err);
+						return callback('error while unsubscribing user ' + kickedUser.id + ' from '+room.name+': '+err);
 
-					return callback(null, room, user, kickedUser, sentEvent);
+					return callback(null, sentEvent);
 				});
 			});
 		},
 
-		function notification(room, user, kickedUser, sentEvent, callback) {
+		function notification(sentEvent, callback) {
 			Notifications(that.app).getType('roomkick').create(kickedUser, room, sentEvent.id, callback);
 		}
 
 	], function(err) {
 		if (err) {
-			logger.error(err);
+			logger.error('[room:kick] ' + err);
 			return next(null, {code: 500, err: err});
 		}
 
