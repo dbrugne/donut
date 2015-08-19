dd8eaf799933716a35bc1b7d2adf3285641b2b37
Yoann Fuks <yoannfuks@hotmail.fr>
2015-08-19 14:26:36 +0200

    #352 prevent default event, create subview for error

--- web-server/public/donut/views/drawer-user-account-mail.js
+++ web-server/public/donut/views/drawer-user-account-mail.js
@@ -4,30 +4,35 @@ define([
   'backbone',
   'client',
   'models/current-user',
+  'views/drawer-user-account-error',
   '_templates'
-], function ($, _, Backbone, client ,currentUser, templates) {
+], function ($, _, Backbone, client ,currentUser, ViewError, templates) {
   var DrawerUserEditMailView = Backbone.View.extend({
 
     template: templates['drawer-user-account-mail.html'],
-
-    errorTemplate: templates['drawer-user-account-error.html'],
 
     events: {
       'submit .form-mail'       : 'onSubmitMail'
     },
 
     initialize: function(options) {
-
     },
 
     render: function() {
       this.$el.html(this.template);
+
+      this.errorView = new ViewError ({
+        el : this.$('.error')
+      });
+
       return this;
     },
 
-    onSubmitMail: function() {
+    onSubmitMail: function(event) {
+      event.preventDefault();
+
       if (this.$('.email-sub').val().length < 1) {
-        this.$('.error').html(this.errorTemplate({error: 'empty'}));
+        this.errorView.render('empty');
         this.$('.form-mail').addClass('has-error');
       } else {
         client.userChangeMail(currentUser.get("user_id"), this.$('.email-sub').val());
--- web-server/public/donut/views/drawer-user-account-password.js
+++ web-server/public/donut/views/drawer-user-account-password.js
@@ -4,13 +4,12 @@ define([
   'backbone',
   'client',
   'models/current-user',
+  'views/drawer-user-account-error',
   '_templates'
-], function ($, _, Backbone, client ,currentUser, templates) {
+], function ($, _, Backbone, client ,currentUser, ViewError, templates) {
   var DrawerUserEditPasswordView = Backbone.View.extend({
 
     template: templates['drawer-user-account-password.html'],
-
-    errorTemplate: templates['drawer-user-account-error.html'],
 
     events: {
       'submit .form-password'  : 'onSubmitPassword'
@@ -22,26 +21,28 @@ define([
 
     render: function() {
       this.$el.html(this.template());
-      this.listenTo(currentUser, 'passwordEdit', this.onPasswordResponse);
+
+      this.errorView = new ViewError ({
+        el : this.$('.error-password'),
+        model: this.model
+      });
+
       return this;
     },
 
-    onPasswordResponse: function () {
-      console.log('test');
-      this.$el.html("response !");
-    },
+    onSubmitPassword: function(event) {
+      event.preventDefault();
 
-    onSubmitPassword: function() {
       if (this.$('.input-password').val() === this.$('.input-password-confirm').val() && this.$('.input-password').val().length >= 6) {
         client.userChangePassword(currentUser.get("user_id"), this.$('.input-password').val());
         this.$('.form-password').html(templates['spinner.html']);
       }
       else if (this.$('.input-password').val().length >= 6) {
         this.$('.form-password').addClass('has-error');
-        this.$('.error-password').html(this.errorTemplate({error: "match"}));
+        this.errorView.render('match');
       } else {
         this.$('.form-password').addClass('has-error');
-        this.$('.error-password').html(this.errorTemplate({error: "length"}));
+        this.errorView.render('length');
       }
     }
 
