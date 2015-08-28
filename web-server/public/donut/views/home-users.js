e78a159627ef7398eaee3d2d803ae05e08ce21f1
Yann <y.lastapis@gmail.com>
2015-08-28 15:43:54 +0200

    #384 move tooltip initialization to correct view

--- web-server/public/donut/views/home-users.js
+++ web-server/public/donut/views/home-users.js
@@ -24,7 +24,22 @@ define([
         search: data.search
       });
       this.$el.html(html);
+
+      this.initializeTooltips();
+
       return this;
+    },
+
+    initializeTooltips: function() {
+      $('[data-toggle="tooltip"][data-type="room-users"]').tooltip({
+        html: true,
+        animation: false,
+        container: 'body',
+        template: '<div class="tooltip tooltip-home-users" role="tooltip"><div class="tooltip-inner"></div></div>',
+        title: function () {
+          return '<div class="username" style="' + this.dataset.bgcolor + '">@' + this.dataset.username + '</div>';
+        }
+      });
     }
   });
 
