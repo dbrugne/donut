0702567b754bf79234dcb9db74daf8c2440bf9b9
dbrugne <damien.brugne@gmail.com>
2015-09-08 18:54:45 +0200

    #375

--- web-server/app/authentication/signup.js
+++ web-server/app/authentication/signup.js
@@ -8,10 +8,14 @@ var bouncer = require('../middlewares/bouncer');
 var validateInput = function (req, res, next) {
   req.checkBody('email', i18next.t('account.email.error.format')).isEmail();
   req.checkBody('password', i18next.t('account.password.error.length')).isLength(6, 50);
+  req.checkBody('username', i18next.t('choose-username.usernameerror')).isUsername();
   if (req.validationErrors()) {
     return res.render('signup', {
       meta: {title: i18next.t('title.default')},
-      userFields: {email: req.body.email},
+      userFields: {
+        email: req.body.email,
+        username: req.body.username
+      },
       errors: req.validationErrors(),
       token: req.csrfToken()
     });
