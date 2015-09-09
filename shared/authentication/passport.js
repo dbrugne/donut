1431f9f0b110f1de1c86a853c38e0122fdc2a22a
dbrugne <damien.brugne@gmail.com>
2015-09-09 10:05:39 +0200

    Lint

--- shared/authentication/passport.js
+++ shared/authentication/passport.js
@@ -22,8 +22,9 @@ var keenIoTracking = function (user, type) {
     }
   };
   keenio.addEvent('user_signup', keenEvent, function (err, res) {
-    if (err)
+    if (err) {
       debug('Error while tracking user_signup in keen.io for ' + user.id + ': ' + err);
+    }
   });
 };
 
@@ -39,23 +40,27 @@ passport.deserializeUser(function (id, done) {
   });
 });
 
-// email and password signup
-passport.use('local-signup', new LocalStrategy({
-  usernameField: 'email', // by default local strategy uses username
+var localStrategyOptions = {
+  usernameField: 'email', // by default LocalStrategy use username
   passwordField: 'password',
   passReqToCallback: true
-},
+};
+
+// email and password signup
+passport.use('local-signup', new LocalStrategy(localStrategyOptions,
   function (req, email, password, done) {
     process.nextTick(function () {
-      // happen if user is already authenticated with another method (e.g.: Facebook)
+      // happen if user is already authenticated with another method (e.g.:
+      // Facebook)
       if (req.user) {
         var user = req.user;
         user.local.email = email;
         user.local.password = user.generateHash(password);
         user.lastlogin_at = Date.now();
         user.save(function (err) {
-          if (err)
+          if (err) {
             throw err;
+          }
 
           return done(null, user);
         });
@@ -64,12 +69,14 @@ passport.use('local-signup', new LocalStrategy({
 
       // find existing user with this email
       email = email.toLowerCase();
-      User.findOne({ 'local.email': email }, function (err, user) {
-        if (err)
+      User.findOne({'local.email': email}, function (err, user) {
+        if (err) {
           return done(err);
+        }
 
-        if (user)
+        if (user) {
           return done(null, false, req.flash('error', i18next.t('account.email.error.alreadyexists')));
+        }
 
         // create
         var newUser = User.getNewUser();
@@ -77,16 +84,18 @@ passport.use('local-signup', new LocalStrategy({
         newUser.local.password = newUser.generateHash(password);
         newUser.lastlogin_at = Date.now();
         newUser.save(function (err) {
-          if (err)
+          if (err) {
             throw err;
+          }
 
           // tracking
           keenIoTracking(newUser, 'email');
 
           // email will be send on next tick but done() is called immediatly
           emailer.welcome(newUser.local.email, function (err) {
-            if (err)
+            if (err) {
               return debug('Unable to sent welcome email: ' + err);
+            }
           });
 
           return done(null, newUser);
@@ -97,19 +106,16 @@ passport.use('local-signup', new LocalStrategy({
   }));
 
 // email and password login
-passport.use('local-login', new LocalStrategy({
-  usernameField: 'email', // by default local strategy uses username
-  passwordField: 'password',
-  passReqToCallback: true
-},
+passport.use('local-login', new LocalStrategy(localStrategyOptions,
   function (req, email, password, done) {
     // find a user whose email is the same as the forms email
     // we are checking to see if the user trying to login already exists
     var searchEmail = email.toLocaleLowerCase();
-    User.findOne({ 'local.email': searchEmail }, function (err, user) {
+    User.findOne({'local.email': searchEmail}, function (err, user) {
       // if there are any errors, return the error before anything else
-      if (err)
+      if (err) {
         return done(err);
+      }
 
       // if no user is found, return the message
       if (!user) {
@@ -126,8 +132,9 @@ passport.use('local-login', new LocalStrategy({
       // all is well, return successful user
       user.lastlogin_at = Date.now();
       user.save(function (err) {
-        if (err)
-          debug(err); // not a problem
+        if (err) {
+          debug(err);
+        } // not a problem
 
         return done(null, user);
       });
@@ -136,32 +143,38 @@ passport.use('local-login', new LocalStrategy({
   }));
 
 // Facebook (Web browser, based on current Facebook session)
-passport.use(new FacebookStrategy({
+var facebookStrategyOptions = {
   clientID: conf.facebook.clientID,
   clientSecret: conf.facebook.clientSecret,
   callbackURL: conf.facebook.callbackURL,
   passReqToCallback: true
-},
+};
+
+passport.use(new FacebookStrategy(facebookStrategyOptions,
   function (req, token, refreshToken, profile, done) {
     process.nextTick(function () {
       if (!req.user) {
-        User.findOne({ 'facebook.id': profile.id }, function (err, user) {
-          if (err)
+        User.findOne({'facebook.id': profile.id}, function (err, user) {
+          if (err) {
             return done(err);
+          }
 
           if (user) {
             user.lastlogin_at = Date.now();
-            // if there is a user id already but no token (user was linked at one point and then removed)
-            // just add our token and profile information
+            // if there is a user id already but no token (user was linked at
+            // one point and then removed) just add our token and profile
+            // information
             if (!user.facebook.token) {
               user.facebook.token = token;
               user.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
-              if (profile.emails)
+              if (profile.emails) {
                 user.facebook.email = profile.emails[0].value;
+              }
             }
             user.save(function (err) {
-              if (err)
+              if (err) {
                 throw err;
+              }
 
               return done(null, user); // user found, return that user
             });
@@ -173,12 +186,14 @@ passport.use(new FacebookStrategy({
             newUser.facebook.token = token;
             newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
             newUser.name = profile.displayName;
-            if (profile.emails)
-              newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+            if (profile.emails) {
+              newUser.facebook.email = profile.emails[0].value;
+            } // facebook can return multiple emails so we'll take the first
 
             newUser.save(function (err) {
-              if (err)
+              if (err) {
                 throw err;
+              }
 
               // tracking
               keenIoTracking(newUser, 'facebook');
@@ -195,25 +210,29 @@ passport.use(new FacebookStrategy({
         var user = req.user;
 
         // Look for another user account that use this identifier
-        User.findOne({ 'facebook.id': profile.id }, function (err, existingUser) {
-          if (err)
+        User.findOne({'facebook.id': profile.id}, function (err, existingUser) {
+          if (err) {
             return done(err);
+          }
 
-          if (existingUser)
+          if (existingUser) {
             return done(null, false,
               req.flash('error', i18next.t('account.facebook.error.alreadylinked')));
+          }
 
           // update the current users facebook credentials
           user.facebook.id = profile.id;
           user.facebook.token = token;
           user.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
-          if (profile.emails)
+          if (profile.emails) {
             user.facebook.email = profile.emails[0].value;
+          }
 
           // save the user
           user.save(function (err) {
-            if (err)
+            if (err) {
               throw err;
+            }
             return done(null, user);
           });
         });
@@ -232,24 +251,27 @@ passport.use(new FacebookTokenStrategy({
   console.log(profile);
 
   var facebookId = profile.id;
-  User.findOne({ 'facebook.id': facebookId }, function (err, user) {
-    if (err)
+  User.findOne({'facebook.id': facebookId}, function (err, user) {
+    if (err) {
       return done(err);
+    }
 
     if (user) {
       user.lastlogin_at = Date.now();
-      // if there is a user id already but no token (user was linked at one point and then removed)
-      // just add our token and profile information
+      // if there is a user id already but no token (user was linked at one
+      // point and then removed) just add our token and profile information
       if (!user.facebook.token) {
         user.facebook.token = accessToken;
 
         user.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
-        if (profile.emails)
+        if (profile.emails) {
           user.facebook.email = profile.emails[0].value;
+        }
       }
       user.save(function (err) {
-        if (err)
+        if (err) {
           return done(err);
+        }
 
         return done(null, user); // user found, return that user
       });
@@ -262,11 +284,13 @@ passport.use(new FacebookTokenStrategy({
       newUser.facebook.token = accessToken;
       newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyName; // @todo :test
       newUser.name = profile.displayName; // @todo :test
-      if (profile.emails)
-        newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+      if (profile.emails) {
+        newUser.facebook.email = profile.emails[0].value;
+      } // facebook can return multiple emails so we'll take the first
       newUser.save(function (err) {
-        if (err)
+        if (err) {
           return done(err);
+        }
 
         // tracking
         keenIoTracking(newUser, 'facebook');
