c2cd99c8fe5c306a273a5f631fa807ca1852b364
dbrugne <damien.brugne@gmail.com>
2015-09-11 15:43:41 +0200

    Lint

--- shared/models/historyone.js
+++ shared/models/historyone.js
@@ -1,9 +1,7 @@
 'use strict';
-var debug = require('debug')('shared:models:historyOne');
 var _ = require('underscore');
 var async = require('async');
 var mongoose = require('../io/mongoose');
-var User = require('./user');
 var cloudinary = require('../util/cloudinary');
 
 var historySchema = mongoose.Schema({
@@ -12,7 +10,8 @@ var historySchema = mongoose.Schema({
   to: { type: mongoose.Schema.ObjectId, ref: 'User' },
   time: { type: Date, default: Date.now },
   data: mongoose.Schema.Types.Mixed,
-  viewed: { type: Boolean, default: false }, // true if to user has read this event
+  viewed: { type: Boolean, default: false }, // true if to user has read this
+                                             // event
   edited: { type: Boolean },
   edited_at: { type: Date }
 
@@ -26,11 +25,11 @@ var dryFields = [
   'from_user_id',
   'from_avatar',
   'to_user_id',
-  'to_username',
+  'to_username'
 ]; // in user:online/offline event the fields are user_id, username, avatar
 
 historySchema.statics.record = function () {
-  var that = this;
+  var Model = this;
   /**
    * @param event - event name as String
    * @param data - event data as Object
@@ -40,7 +39,7 @@ historySchema.statics.record = function () {
   return function (event, data, fn) {
     // user:online/offline special case
     var fromUserId, toUserId;
-    if (data.from_user_id == undefined && data.from) {
+    if (data.from_user_id === undefined && data.from) {
       fromUserId = data.from;
       toUserId = data.to;
     } else {
@@ -48,7 +47,7 @@ historySchema.statics.record = function () {
       toUserId = data.to_user_id;
     }
 
-    var model = new that();
+    var model = new Model();
     model.event = event;
     model.from = fromUserId;
     model.to = toUserId;
@@ -59,12 +58,12 @@ historySchema.statics.record = function () {
     model.data = _.omit(wet, dryFields);
 
     model.save(function (err) {
-      if (err)
+      if (err) {
         return fn('Unable to save historyOne ' + model.from + '=>' + model.to + ': ' + err);
+      }
 
       return fn(null, model);
     });
-
   };
 };
 
@@ -92,21 +91,24 @@ historySchema.methods.toClientJSON = function (userViewed) {
     data.to_username = this.to.username;
     data.to_avatar = this.to._avatar();
   }
-  if (this.edited === true)
+  if (this.edited === true) {
     e.edited = this.edited;
+  }
 
   // images
   if (data.images && data.images.length > 0) {
     data.images = _.map(data.images, function (element, key, value) {
-      // @important: use .path to obtain URL with file extension and avoid CORS errors
+      // @important: use .path to obtain URL with file extension and avoid CORS
+      // errors
       return cloudinary.messageImage(element.path);
     });
   }
 
   e.data = data;
 
-  // unviewed status (true if message, i'm the receiver and current value is false)
-  if (userViewed && ['user:message', 'user:me'].indexOf(this.event) !== -1 && data.to_user_id == userViewed && !this.viewed) {
+  // unviewed status (true if message, i'm the receiver and current value is
+  // false)
+  if (userViewed && [ 'user:message', 'user:me' ].indexOf(this.event) !== -1 && data.to_user_id === userViewed && !this.viewed) {
     e.unviewed = true;
   }
 
@@ -125,8 +127,8 @@ historySchema.statics.retrieve = function () {
     what = what || {};
     var criteria = {
       $or: [
-        {from: me, to: other},
-        {from: other, to: me}
+        { from: me, to: other },
+        { from: other, to: me }
       ]
     };
 
@@ -141,20 +143,21 @@ historySchema.statics.retrieve = function () {
     var limit = howMany + 1;
 
     var q = that.find(criteria)
-      .sort({time: 'desc'}) // important for timeline logic but also optimize rendering on frontend
+      .sort({ time: 'desc' }) // important for timeline logic but also optimize
+                              // rendering on frontend
       .limit(limit)
       .populate('from', 'username avatar color facebook')
       .populate('to', 'username avatar color facebook');
 
     q.exec(function (err, entries) {
-      if (err)
+      if (err) {
         return fn('Error while retrieving room history: ' + err);
+      }
 
-      var more = (entries.length > howMany)
-        ? true
-        : false;
-      if (more)
-        entries.pop(); // remove last
+      var more = (entries.length > howMany);
+      if (more) {
+        entries.pop();
+      } // remove last
 
       var history = [];
       _.each(entries, function (entry) {
@@ -173,14 +176,17 @@ historySchema.statics.retrieve = function () {
  * Retrieve an history event context and return a JSON representation
  *
  * @param eventId     Event ID to retrieve
- * @param limit       Number of maximum event to return in both before and after direction
- * @param timeLimit   Limit of time to retrieve event in both before and after direction
+ * @param limit       Number of maximum event to return in both before and
+ *   after direction
+ * @param timeLimit   Limit of time to retrieve event in both before and after
+ *   direction
  * @param before      Flag to retrieve (or not) the "before event events"
  * @param fn
  */
 historySchema.statics.retrieveEventWithContext = function (eventId, limit, timeLimit, before, fn) {
-  if (!eventId)
+  if (!eventId) {
     return fn('retrieveEventWithContext expect event ID as first parameter');
+  }
 
   limit = limit || 5;
   timeLimit = timeLimit || 5; // in mn
@@ -189,7 +195,7 @@ historySchema.statics.retrieveEventWithContext = function (eventId, limit, timeL
   var criteria = {
     _id: { $ne: eventId },
     $or: [],
-    event: { $in: ['user:message', 'user:me'] }
+    event: { $in: [ 'user:message', 'user:me' ] }
   };
 
   var model;
@@ -198,13 +204,13 @@ historySchema.statics.retrieveEventWithContext = function (eventId, limit, timeL
   async.waterfall([
 
     function retrieveModel (callback) {
-      that.findOne({_id: eventId})
+      that.findOne({ _id: eventId })
         .populate('from', 'username avatar color facebook')
         .populate('to', 'username avatar color facebook')
         .exec(function (err, event) {
           model = event;
-          criteria.$or.push({from: event.from, to: event.to});
-          criteria.$or.push({from: event.to, to: event.from});
+          criteria.$or.push({ from: event.from, to: event.to });
+          criteria.$or.push({ from: event.to, to: event.from });
           fullResults.push(model);
           return callback(err);
         });
@@ -213,15 +219,16 @@ historySchema.statics.retrieveEventWithContext = function (eventId, limit, timeL
       var afterLimit = new Date(model.time);
       afterLimit.setMinutes(afterLimit.getMinutes() + timeLimit);
       var _criteria = _.clone(criteria);
-      _criteria.time = {$lte: afterLimit, $gte: model.time};
+      _criteria.time = { $lte: afterLimit, $gte: model.time };
       that.find(_criteria)
-        .sort({time: 'asc'})
+        .sort({ time: 'asc' })
         .limit(limit)
         .populate('from', 'username avatar color facebook')
         .populate('to', 'username avatar color facebook')
         .exec(function (err, results) {
-          if (err)
+          if (err) {
             return callback(err);
+          }
 
           fullResults = fullResults.concat(results);
           return callback(null);
@@ -229,21 +236,23 @@ historySchema.statics.retrieveEventWithContext = function (eventId, limit, timeL
     },
 
     function retrieveBeforeEvents (callback) {
-      if (!before)
+      if (!before) {
         return callback(null);
+      }
 
       var beforeLimit = new Date(model.time);
       beforeLimit.setMinutes(beforeLimit.getMinutes() - timeLimit);
       var _criteria = _.clone(criteria);
       _criteria.time = { $gte: beforeLimit, $lte: model.time };
       that.find(_criteria)
-        .sort({time: 'desc'})
+        .sort({ time: 'desc' })
         .limit(limit)
         .populate('from', 'username avatar color facebook')
         .populate('to', 'username avatar color facebook')
         .exec(function (err, results) {
-          if (err)
+          if (err) {
             return callback(err);
+          }
 
           results.reverse();
           fullResults = results.concat(fullResults);
@@ -262,7 +271,6 @@ historySchema.statics.retrieveEventWithContext = function (eventId, limit, timeL
   ], function (err, history) {
     fn(err, history);
   });
-
 };
 
 module.exports = mongoose.model('HistoryOne', historySchema, 'history-one');
