b7fd69da0616b9ff93fa5093f6b6daf6170188ab
dbrugne <damien.brugne@gmail.com>
2015-09-11 18:41:56 +0200

    Lint

--- tasks/missing-translations.js
+++ tasks/missing-translations.js
@@ -1,21 +1,24 @@
-var async = require('async');
 var _ = require('underscore');
 
-module.exports = function(grunt) {
-  grunt.registerTask('donut-missing-translations', function() {
+module.exports = function (grunt) {
+  grunt.registerTask('donut-missing-translations', function () {
     var en = require('../locales/en/translation');
     var fr = require('../locales/fr/translation');
 
-    var analyseTree = function(subject, compare, label) {
-      var processNode = function(element, key) {
-        if (_.isObject(element) && !_.isArray(element))
-          return _.each(element, function(_element, _key, _list) {
-            var path = (key) ? key + '.' + _key : _key; // root
+    var analyseTree = function (subject, compare, label) {
+      var processNode = function (element, key) {
+        if (_.isObject(element) && !_.isArray(element)) {
+          return _.each(element, function (_element, _key, _list) {
+            var path = (key)
+              ? key + '.' + _key
+              : _key; // root
             processNode(_element, path);
           });
+        }
 
-        if (!key)
-          return; // root
+        if (!key) {
+          return;
+        } // root
 
         // Missing array special case
         var pattern = /(\.[0-9]+$)/;
@@ -25,22 +28,24 @@ module.exports = function(grunt) {
 
         var steps = key.split('.');
         steps.unshift(compare);
-        if (!checkNested.apply(this, steps))
+        if (!checkNested.apply(this, steps)) {
           grunt.log.error('Missing key in ' + label + ': ' + key);
+        }
       };
 
-      // @source: http://stackoverflow.com/questions/2631001/javascript-test-for-existence-of-nested-object-key
-      function checkNested(obj) {
+      // @source:
+      // http://stackoverflow.com/questions/2631001/javascript-test-for-existence-of-nested-object-key
+      function checkNested (obj) {
         var args = Array.prototype.slice.call(arguments, 1);
 
         for (var i = 0; i < args.length; i++) {
-          var key = args[i];
+          var key = args[ i ];
 
           if (!obj || !obj.hasOwnProperty(key)) {
             return false;
           }
 
-          obj = obj[key];
+          obj = obj[ key ];
         }
         return true;
       }
