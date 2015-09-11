01e26f1a5ebcbe28b9965d0c0034fbb0a65bdb64
dbrugne <damien.brugne@gmail.com>
2015-09-11 18:33:50 +0200

    Lint

--- mongo/message-number-by-day.js
+++ mongo/message-number-by-day.js
@@ -1,8 +1,8 @@
-db['history-room'].aggregate([
+db[ 'history-room' ].aggregate([
   { $match: { event: 'room:message' } },
   {
-    $group : {
-      _id : { month: { $month: "$time" }, day: { $dayOfMonth: "$time" } },
+    $group: {
+      _id: { month: { $month: '$time' }, day: { $dayOfMonth: '$time' } },
       count: { $sum: 1 }
     }
   }
