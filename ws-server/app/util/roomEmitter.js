6f68f5dcf61f52e72463079bd41b2d8552668d2b
dbrugne <damien.brugne@gmail.com>
2015-09-11 15:41:44 +0200

    Lint

--- ws-server/app/util/roomEmitter.js
+++ ws-server/app/util/roomEmitter.js
@@ -1,8 +1,5 @@
 'use strict';
-var logger = require('../../pomelo-logger').getLogger('donut', __filename);
-var debug = require('debug')('donut:server:ws:room-emitter');
 var _ = require('underscore');
-var async = require('async');
 var recorder = require('../../../shared/models/historyroom').record();
 var UserModel = require('../../../shared/models/user');
 var cloudinary = require('../../../shared/util/cloudinary');
@@ -13,17 +10,19 @@ var cloudinary = require('../../../shared/util/cloudinary');
  *   callback(err, sentEvent)
  *
  * @param app
- * @param Room
- * @param User that made the action
+ * @param user
+ * @param room
  * @param eventName
  * @param eventData
  * @param callback
  */
 module.exports = function (app, user, room, eventName, eventData, callback) {
-  if (!room)
+  if (!room) {
     return callback('roomEmitter require room parameter');
-  if (!user)
+  }
+  if (!user) {
     return callback('roomEmitter require user parameter');
+  }
 
   eventData.time = Date.now();
   eventData.name = room.name;
@@ -31,8 +30,9 @@ module.exports = function (app, user, room, eventName, eventData, callback) {
   eventData.room_id = room.id;
 
   recorder(room, eventName, eventData, function (err, model) {
-    if (err)
-      return fn('Error while emitting room event ' + eventName + ' in ' + room.name + ': ' + err);
+    if (err) {
+      return callback('Error while emitting room event ' + eventName + ' in ' + room.name + ': ' + err);
+    }
 
     eventData.id = model.id;
 
@@ -40,17 +40,20 @@ module.exports = function (app, user, room, eventName, eventData, callback) {
     // images
     if (eventData.images && eventData.images.length > 0) {
       eventData.images = _.map(eventData.images, function (element, key, value) {
-        // @important: use .path to obtain URL with file extension and avoid CORS errors
+        // @important: use .path to obtain URL with file extension and avoid
+        // CORS errors
         return cloudinary.messageImage(element.path);
       });
     }
 
     app.globalChannelService.pushMessage('connector', eventName, eventData, room.name, {}, function (err) {
-      if (err)
+      if (err) {
         return callback('Error while pushing message: ' + err);
+      }
 
-      if (['room:message', 'room:topic', 'room:me'].indexOf(eventName) === -1)
+      if ([ 'room:message', 'room:topic', 'room:me' ].indexOf(eventName) === -1) {
         return callback(null, eventData);
+      }
 
       // set unviewed flag on users
       UserModel.setUnviewedRoomMessage(room._id, room.users, user._id, model._id, function (err) {
@@ -58,5 +61,4 @@ module.exports = function (app, user, room, eventName, eventData, callback) {
       });
     });
   });
-
 };
