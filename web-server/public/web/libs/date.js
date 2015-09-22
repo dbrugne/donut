38f5d83a2324c3e0cda53c191981f1f534e4f912
Sebastien Pariaud <sebastienpariaud@gmail.com>
2015-09-22 18:01:45 +0200

    refactor lib date.js
    #506

--- web-server/public/web/libs/date.js
+++ web-server/public/web/libs/date.js
@@ -1,11 +1,10 @@
 var i18next = require('i18next-client');
+var $ = require('jquery');
 
 module.exports = {
   longDate: function (date) {
     var myDate = new Date(date);
-    var day = myDate.getDay();
-    var month = myDate.getMonth();
-    return i18next.t('date.days.' + day) + ' ' + myDate.getDate() + ' ' + i18next.t('date.months.' + month) + ' ' + myDate.getFullYear();
+    return i18next.t('date.days.' + myDate.getDay()) + ' ' + myDate.getDate() + ' ' + i18next.t('date.months.' + myDate.getMonth()) + ' ' + myDate.getFullYear();
   },
   dateTime: function (date) {
     var myDate = new Date(date);
@@ -15,9 +14,7 @@ module.exports = {
       ' ' +
       myDate.getFullYear() +
       ', ' +
-      myDate.getHours() +
-      ':' +
-      myDate.getMinutes() +
+      this.shortTime(date) +
       ':' +
       myDate.getSeconds();
   },
@@ -31,9 +28,7 @@ module.exports = {
       ' ' +
       myDate.getFullYear() +
       ', ' +
-      myDate.getHours() +
-      ':' +
-      myDate.getMinutes() +
+      this.shortTime(date) +
       ':' +
       myDate.getSeconds();
   },
@@ -45,32 +40,46 @@ module.exports = {
     var myDate = new Date(date);
     var dayDate = myDate.getDate();
     var monthDate = (myDate.getMonth() + 1);
-    return ((dayDate < 10) ? ('0' + dayDate) : dayDate) +
-      '/' +
-      ((monthDate < 10) ? ('0' + monthDate) : monthDate);
+    return ((dayDate < 10) ? ('0' + dayDate) : dayDate) + '/' + ((monthDate < 10) ? ('0' + monthDate) : monthDate);
+  },
+  shortDayMonthTime: function (date) {
+    var myDate = new Date(date);
+    return myDate.getDate() + ' ' + i18next.t('date.months.' + myDate.getMonth()) + ', ' + this.shortTime(date);
   },
   shortMonthYear: function (date) {
     var myDate = new Date(date);
     var monthDate = (myDate.getMonth() + 1);
-    return ((monthDate < 10) ? ('0' + monthDate) : monthDate) +
-      '/' +
-      myDate.getFullYear();
+    return ((monthDate < 10) ? ('0' + monthDate) : monthDate) + '/' + myDate.getFullYear();
   },
   diffInDays: function (date) {
     var myDate = new Date(date);
     var currentDate = new Date();
-    var days = currentDate.getTime() - myDate.getTime();
-    return Math.ceil(days / (1000 * 60 * 60 * 24));
+    return Math.ceil((currentDate.getTime() - myDate.getTime()) / (1000 * 60 * 60 * 24));
   },
+  from: function (format, $element) {
+    format = format || 'fromnow';
 
+    var element = $($element);
+    var time = element.attr('data-time');
+    if (!time) return;
+
+    if (format === 'fromnow') {
+      //element.text(dateObject.fromNow());
+      return;
+    } else if (format === 'date') {
+      element.text(this.longDate(time));
+    }
+    element.attr('title', this.longDateTime(time));
+  },
   test: function () {
     console.log('longDate(2015-09-02T13:52:13.083Z) :  mercredi 2 septembre 2015 >> ' + this.longDate('2015-09-02T13:52:13.083Z'));
     console.log('dateTime(2015-09-02T13:52:13.083Z) :  2 septembre 2015, 15:52:13 >> ' + this.dateTime('2015-09-02T13:52:13.083Z'));
     console.log('longDateTime(2015-09-02T13:52:13.083Z) :  mercredi 2 septembre 2015, 15:52:13 >> ' + this.longDateTime('2015-09-02T13:52:13.083Z'));
     console.log('shortTime(2015-09-02T13:52:13.083Z) :  15:52 >> ' + this.shortTime('2015-09-02T13:52:13.083Z'));
     console.log('shortDayMonth(2015-09-02T13:52:13.083Z) :  02/09 >> ' + this.shortDayMonth('2015-09-02T13:52:13.083Z'));
+    console.log('shortDayMonthTime(2015-09-02T13:52:13.083Z) :  2 septembre, 15:52 >> ' + this.shortDayMonthTime('2015-09-02T13:52:13.083Z'));
     console.log('shortMonthYear(2015-09-02T13:52:13.083Z) :  09/2015 >> ' + this.shortMonthYear('2015-09-02T13:52:13.083Z'));
-    console.log('diffInDays(2015-09-02T13:52:13.083Z) : 20 >> ' + this.diffInDays('2015-09-02T13:52:13.083Z'));
+    console.log('diffInDays(2015-09-02T13:52:13.083Z) : 21 >> ' + this.diffInDays('2015-09-02T13:52:13.083Z'));
   }
 };
 
