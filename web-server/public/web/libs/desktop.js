e4204e7c1ff2637e292f8e63c89bb307d950dc7c
dbrugne <damien.brugne@gmail.com>
2015-09-23 11:03:58 +0200

    Simplify, lint, cleanup and use underscore.
    #520

--- web-server/public/web/libs/desktop.js
+++ web-server/public/web/libs/desktop.js
@@ -1,211 +1,198 @@
 /**
- * Module inspired by the no longer maintained ttsvetko/HTML5-Desktop-Notifications
+ * Module inspired by the no longer maintained
+ * ttsvetko/HTML5-Desktop-Notifications
  *
  * source: https://github.com/ttsvetko/HTML5-Desktop-Notifications/
+ *
+ * /!\ Warning: Safari native methods required for Notifications do NOT run in
+ * strict mode.
  */
 
-/**
- * Copyright 2012 Tsvetan Tsvetkov
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Author: Tsvetan Tsvetkov (tsekach@gmail.com)
- */
-(function (win) {
+var _ = require('underscore');
+
+var PERMISSION_DEFAULT = 'default';
+var PERMISSION_GRANTED = 'granted';
+var PERMISSION_DENIED = 'denied';
+var PERMISSION = [ PERMISSION_GRANTED, PERMISSION_DEFAULT, PERMISSION_DENIED ];
+var defaultSetting = {
+  pageVisibility: true, // always display, even if application is in background
+  autoClose: 3000
+};
+var emptyString = '';
+
+var isSupported = (function () {
+  var isSupported = false;
   /*
-   Safari native methods required for Notifications do NOT run in strict mode.
+   * Use try {} catch() {} because the check for IE may throws an exception
+   * if the code is run on browser that is not Safari/Chrome/IE or
+   * Firefox with html5notifications plugin.
+   *
+   * Also, we canNOT detect if msIsSiteMode method exists, as it is
+   * a method of host object. In IE check for existing method of host
+   * object returns undefined. So, we try to run it - if it runs
+   * successfully - then it is IE9+, if not - an exceptions is thrown.
    */
-  //"use strict";
-  var PERMISSION_DEFAULT = "default",
-    PERMISSION_GRANTED = "granted",
-    PERMISSION_DENIED = "denied",
-    PERMISSION = [PERMISSION_GRANTED, PERMISSION_DEFAULT, PERMISSION_DENIED],
-    defaultSetting = {
-      pageVisibility: false,
-      autoClose: 0
-    },
-    empty = {},
-    emptyString = "",
-    isSupported = (function () {
-      var isSupported = false;
-      /*
-       * Use try {} catch() {} because the check for IE may throws an exception
-       * if the code is run on browser that is not Safar/Chrome/IE or
-       * Firefox with html5notifications plugin.
-       *
-       * Also, we canNOT detect if msIsSiteMode method exists, as it is
-       * a method of host object. In IE check for existing method of host
-       * object returns undefined. So, we try to run it - if it runs
-       * successfully - then it is IE9+, if not - an exceptions is thrown.
-       */
-      try {
-        isSupported = !!(/* Safari, Chrome */win.Notification || /* Chrome & ff-html5notifications plugin */win.webkitNotifications || /* Firefox Mobile */navigator.mozNotification || /* IE9+ */(win.external && win.external.msIsSiteMode() !== undefined));
-      } catch (e) {}
-      return isSupported;
-    }()),
-    ieVerification = Math.floor((Math.random() * 10) + 1),
-    isFunction = function (value) { return (value && (value).constructor === Function); },
-    isString = function (value) {return (value && (value).constructor === String); },
-    isObject = function (value) {return (value && (value).constructor === Object); },
-    /**
-     * Dojo Mixin
-     */
-    mixin = function (target, source) {
-      var name, s;
-      for (name in source) {
-        s = source[name];
-        if (!(name in target) || (target[name] !== s && (!(name in empty) || empty[name] !== s))) {
-          target[name] = s;
-        }
-      }
-      return target; // Object
-    },
-    noop = function () {},
-    settings = defaultSetting;
-  function getNotification(title, options) {
-    var notification;
-    if (win.Notification) { /* Safari 6, Chrome (23+) */
-      notification =  new win.Notification(title, {
-        /* The notification's icon - For Chrome in Windows, Linux & Chrome OS */
-        icon: isString(options.icon) ? options.icon : options.icon.x32,
-        /* The notification’s subtitle. */
-        body: options.body || emptyString,
-        /*
-         The notification’s unique identifier.
-         This prevents duplicate entries from appearing if the user has multiple instances of your website open at once.
-         */
-        tag: options.tag || emptyString
-      });
-    } else if (win.webkitNotifications) { /* FF with html5Notifications plugin installed */
-      notification = win.webkitNotifications.createNotification(options.icon, title, options.body);
-      notification.show();
-    } else if (navigator.mozNotification) { /* Firefox Mobile */
-      notification = navigator.mozNotification.createNotification(title, options.body, options.icon);
-      notification.show();
-    } else if (win.external && win.external.msIsSiteMode()) { /* IE9+ */
-      //Clear any previous notifications
-      win.external.msSiteModeClearIconOverlay();
-      win.external.msSiteModeSetIconOverlay((isString(options.icon) ? options.icon : options.icon.x16), title);
-      win.external.msSiteModeActivate();
-      notification = {
-        "ieVerification": ieVerification + 1
-      };
-    }
-    return notification;
+  try {
+    isSupported = !!(/* Safari, Chrome */window.Notification || /* Chrome & ff-html5notifications plugin */window.webkitNotifications || /* Firefox Mobile */navigator.mozNotification || /* IE9+ */(window.external && window.external.msIsSiteMode() !== undefined));
+  } catch (e) {
   }
-  function getWrapper(notification) {
-    return {
-      close: function () {
-        if (notification) {
-          if (notification.close) {
-            //http://code.google.com/p/ff-html5notifications/issues/detail?id=58
-            notification.close();
-          }
-          else if (notification.cancel) {
-            notification.cancel();
-          } else if (win.external && win.external.msIsSiteMode()) {
-            if (notification.ieVerification === ieVerification) {
-              win.external.msSiteModeClearIconOverlay();
-            }
-          }
-        }
-      }
+  return isSupported;
+}());
+var ieVerification = Math.floor((Math.random() * 10) + 1);
+
+var settings = defaultSetting;
+
+function getNotification (title, options) {
+  var notification;
+  if (window.Notification) { /* Safari 6, Chrome (23+) */
+    notification = new window.Notification(title, {
+      /* The notification's icon - For Chrome in Windows, Linux & Chrome OS */
+      icon: _.isString(options.icon)
+        ? options.icon
+        : options.icon.x32,
+      /* The notification’s subtitle. */
+      body: options.body || emptyString,
+      /*
+       The notification’s unique identifier.
+       This prevents duplicate entries from appearing if the user has multiple instances of your website open at once.
+       */
+      tag: options.tag || emptyString
+    });
+  } else if (window.webkitNotifications) { /* FF with html5Notifications plugin installed */
+    notification = window.webkitNotifications.createNotification(options.icon, title, options.body);
+    notification.show();
+  } else if (navigator.mozNotification) { /* Firefox Mobile */
+    notification = navigator.mozNotification.createNotification(title, options.body, options.icon);
+    notification.show();
+  } else if (window.external && window.external.msIsSiteMode()) { /* IE9+ */
+    // Clear any previous notifications
+    window.external.msSiteModeClearIconOverlay();
+    window.external.msSiteModeSetIconOverlay((_.isString(options.icon)
+      ? options.icon
+      : options.icon.x16), title);
+    window.external.msSiteModeActivate();
+    notification = {
+      'ieVerification': ieVerification + 1
     };
   }
-  function requestPermission(callback) {
-    if (!isSupported) { return; }
-    var callbackFunction = isFunction(callback) ? callback : noop;
-    if (win.webkitNotifications && win.webkitNotifications.checkPermission) {
-      /*
-       * Chrome 23 supports win.Notification.requestPermission, but it
-       * breaks the browsers, so use the old-webkit-prefixed
-       * win.webkitNotifications.checkPermission instead.
-       *
-       * Firefox with html5notifications plugin supports this method
-       * for requesting permissions.
-       */
-      win.webkitNotifications.requestPermission(callbackFunction);
-    } else if (win.Notification && win.Notification.requestPermission) {
-      win.Notification.requestPermission(callbackFunction);
+  if (_.isFunction(options.onclick)) {
+    notification.onclick = options.onclick;
+  }
+  if (_.isFunction(options.onclose)) {
+    notification.onclose = options.onclose;
+  }
+  if (_.isFunction(options.ondisplay)) {
+    if (window.Notification) {
+      notification.onshow = options.ondisplay;
+    } else {
+      notification.ondisplay = options.ondisplay;
     }
   }
-  function permissionLevel() {
-    var permission;
-    if (!isSupported) { return; }
-    if (win.Notification && win.Notification.permissionLevel) {
-      //Safari 6
-      permission = win.Notification.permissionLevel();
-    } else if (win.webkitNotifications && win.webkitNotifications.checkPermission) {
-      //Chrome & Firefox with html5-notifications plugin installed
-      permission = PERMISSION[win.webkitNotifications.checkPermission()];
-    } else if (win.Notification && win.Notification.permission) {
-      // Firefox 23+
-      permission = win.Notification.permission;
-    } else if (navigator.mozNotification) {
-      //Firefox Mobile
-      permission = PERMISSION_GRANTED;
-    } else if (win.external && (win.external.msIsSiteMode() !== undefined)) { /* keep last */
-      //IE9+
-      permission = win.external.msIsSiteMode() ? PERMISSION_GRANTED : PERMISSION_DEFAULT;
-    }
-    return permission;
+  if (_.isFunction(options.onerror)) {
+    notification.onerror = options.onerror;
   }
-  /**
-   *
-   */
-  function config(params) {
-    if (params && isObject(params)) {
-      mixin(settings, params);
-    }
-    return settings;
-  }
+  return notification;
+}
 
-  function createNotification(title, options) {
-    var notification,
-      notificationWrapper;
-    /*
-     Return undefined if notifications are not supported.
-
-     Return undefined if no permissions for displaying notifications.
-
-     Title and icons are required. Return undefined if not set.
-     */
-    if (isSupported && isString(title) && (options && (isString(options.icon) || isObject(options.icon))) && (permissionLevel() === PERMISSION_GRANTED)) {
-      notification = getNotification(title, options);
-    }
-    notificationWrapper = getWrapper(notification);
-    //Auto-close notification
-    if (settings.autoClose && notification && !notification.ieVerification && notification.addEventListener) {
-      notification.addEventListener("show", function () {
-        var notification = notificationWrapper;
-        win.setTimeout(function () {
+function getWrapper (notification) {
+  return {
+    close: function () {
+      if (notification) {
+        if (notification.close) {
+          // http://code.google.com/p/ff-html5notifications/issues/detail?id=58
           notification.close();
-        }, settings.autoClose);
-      });
+        } else if (notification.cancel) {
+          notification.cancel();
+        } else if (window.external && window.external.msIsSiteMode()) {
+          if (notification.ieVerification === ieVerification) {
+            window.external.msSiteModeClearIconOverlay();
+          }
+        }
+      }
     }
-    return notificationWrapper;
-  }
-  win.notify = {
-    PERMISSION_DEFAULT: PERMISSION_DEFAULT,
-    PERMISSION_GRANTED: PERMISSION_GRANTED,
-    PERMISSION_DENIED: PERMISSION_DENIED,
-    isSupported: isSupported,
-    config: config,
-    createNotification: createNotification,
-    permissionLevel: permissionLevel,
-    requestPermission: requestPermission
   };
-  if (isFunction(Object.seal)) {
-    Object.seal(win.notify);
+}
+
+function requestPermission (callback) {
+  if (!isSupported) {
+    return;
   }
-}(window));
+  var callbackFunction = _.isFunction(callback)
+    ? callback
+    : _.noop;
+  if (window.webkitNotifications && window.webkitNotifications.checkPermission) {
+    /*
+     * Chrome 23 supports window.Notification.requestPermission, but it
+     * breaks the browsers, so use the old-webkit-prefixed
+     * window.webkitNotifications.checkPermission instead.
+     *
+     * Firefox with html5notifications plugin supports this method
+     * for requesting permissions.
+     */
+    window.webkitNotifications.requestPermission(callbackFunction);
+  } else if (window.Notification && window.Notification.requestPermission) {
+    window.Notification.requestPermission(callbackFunction);
+  }
+}
+
+function permissionLevel () {
+  var permission;
+  if (!isSupported) {
+    return;
+  }
+  if (window.Notification && window.Notification.permissionLevel) {
+    // Safari 6
+    permission = window.Notification.permissionLevel();
+  } else if (window.webkitNotifications && window.webkitNotifications.checkPermission) {
+    // Chrome & Firefox with html5-notifications plugin installed
+    permission = PERMISSION[ window.webkitNotifications.checkPermission() ];
+  } else if (window.Notification && window.Notification.permission) {
+    // Firefox 23+
+    permission = window.Notification.permission;
+  } else if (navigator.mozNotification) {
+    // Firefox Mobile
+    permission = PERMISSION_GRANTED;
+  } else if (window.external && (window.external.msIsSiteMode() !== undefined)) { /* keep last */
+    // IE9+
+    permission = window.external.msIsSiteMode()
+      ? PERMISSION_GRANTED
+      : PERMISSION_DEFAULT;
+  }
+  return permission;
+}
+
+function createNotification (title, options) {
+  var notification;
+  var notificationWrapper;
+  /**
+   * Return undefined if notifications are not supported.
+   * Return undefined if no permissions for displaying notifications.
+   * Title and icons are required. Return undefined if not set.
+   */
+  if (isSupported && _.isString(title) && (options &&
+    (_.isString(options.icon) || _.isObject(options.icon))) &&
+    (permissionLevel() === PERMISSION_GRANTED)) {
+    notification = getNotification(title, options);
+  }
+  notificationWrapper = getWrapper(notification);
+  // Auto-close notification
+  if (settings.autoClose && notification && !notification.ieVerification && notification.addEventListener) {
+    notification.addEventListener('show', function () {
+      var notification = notificationWrapper;
+      window.setTimeout(function () {
+        notification.close();
+      }, settings.autoClose);
+    });
+  }
+  return notificationWrapper;
+}
+
+module.exports = {
+  PERMISSION_DEFAULT: PERMISSION_DEFAULT,
+  PERMISSION_GRANTED: PERMISSION_GRANTED,
+  PERMISSION_DENIED: PERMISSION_DENIED,
+  isSupported: isSupported,
+  createNotification: createNotification,
+  permissionLevel: permissionLevel,
+  requestPermission: requestPermission
+};
