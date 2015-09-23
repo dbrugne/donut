d1698279a9bce103a218903f7627c87595d79460
Yann <y.lastapis@gmail.com>
2015-09-23 16:19:15 +0200

    #518

--- web-server/public/web/views/modal-welcome.js
+++ web-server/public/web/views/modal-welcome.js
@@ -33,6 +33,7 @@ var WelcomeModalView = Backbone.View.extend({
     var rooms = [];
     _.each(welcome.featured, function (room) {
       room.avatar = common.cloudinary.prepare(room.avatar, 135);
+      room.join = '/!#room/' + room.name.replace('#', '');
       rooms.push(room);
     });
 
