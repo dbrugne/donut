cb50bd60a6335e4ae0871494caacdc06bb6646e6
dbrugne <damien.brugne@gmail.com>
2015-09-25 18:18:44 +0200

    Use debug dep browser lib

--- web-server/public/web/libs/donut-debug.js
+++ web-server/public/web/libs/donut-debug.js
@@ -1,5 +1,5 @@
 var _ = require('underscore');
-var debug = require('debug');
+var debug = require('debug/browser');
 
 window.dd = debug; // @debug
 
@@ -10,28 +10,32 @@ var donutDebug = function (namespace) {
 
   if (debug.enabled(namespace)) {
     dbg.start = function (name) {
-      if (this.isOn === true)
-        times[name] = Date.now();
+      if (this.isOn === true) {
+        times[ name ] = Date.now();
+      }
     };
     dbg.end = function (name) {
-      if (this.isOn !== true)
+      if (this.isOn !== true) {
         return;
+      }
 
-      if (!_.has(times, name))
+      if (!_.has(times, name)) {
         return;
+      }
 
       var end = Date.now();
-      var start = times[name];
+      var start = times[ name ];
       this("[profiling] Duration of '" + name + "': " + (end - start) + 'ms');
-      delete times[name];
+      delete times[ name ];
     };
   } else {
-    dbg.start = function () {};
-    dbg.end = function () {};
+    dbg.start = function () {
+    };
+    dbg.end = function () {
+    };
   }
 
   return dbg;
 };
-
 
 module.exports = donutDebug;
