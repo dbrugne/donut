778147e08aadb475383cf3c1edf9d782ea8318a5
dbrugne <damien.brugne@gmail.com>
2015-09-28 18:24:25 +0200

    Avoid password sending to every user in roomReadHandler. Reduce data size and simplify code for users list.

--- web-server/public/web/views/drawer-room-profile.js
+++ web-server/public/web/views/drawer-room-profile.js
@@ -53,7 +53,9 @@ var DrawerRoomProfileView = Backbone.View.extend({
 
     room.url = '/room/' + room.name.replace('#', '').toLocaleLowerCase();
 
-    this._users(room); // decorate room object with users_list
+    _.each(room.users, function (element, key, list) {
+      element.avatar = common.cloudinary.prepare(element.avatar, 34);
+    });
 
     var html = this.template({room: room});
     this.$el.html(html);
@@ -64,59 +66,6 @@ var DrawerRoomProfileView = Backbone.View.extend({
     }
 
     this.initializeTooltips();
-  },
-  /**
-   * Construct the room users list for profile displaying
-   * For each set user_id, username, avatar and color
-   * @param room
-   * @private
-   */
-  _users: function (room) {
-    var list = [];
-
-    var alreadyIn = [];
-
-    function pushNew (user, owner, op) {
-      if (!user.user_id) {
-        return;
-      }
-
-      if (alreadyIn.indexOf(user.user_id) !== -1) {
-        return;
-      } else {
-        alreadyIn.push(user.user_id);
-      }
-
-      if (owner === true) {
-        user.isOwner = true;
-      }
-
-      if (op === true) {
-        user.isOp = true;
-      }
-
-      user.avatar = common.cloudinary.prepare(user.avatar, 34);
-
-      list.push(user);
-    }
-
-    if (room.owner) {
-      pushNew(room.owner, true, false);
-    }
-
-    if (room.op && room.op.length > 0) {
-      _.each(room.op, function (user) {
-        pushNew(user, false, true);
-      });
-    }
-
-    if (room.users && room.users.length > 0) {
-      _.each(room.users, function (user) {
-        pushNew(user, false, false);
-      });
-    }
-
-    room.users_list = list;
   },
   initializeTooltips: function () {
     this.$el.find('[data-toggle="tooltip"][data-type="mode"]').tooltip({
