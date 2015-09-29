824dd18c6d7b3c82f42ba6bf59af2a943929cf76
dbrugne <damien.brugne@gmail.com>
2015-09-29 10:58:21 +0200

    Remove obsolete files (https://github.com/dbrugne/donut/wiki/MongoDB-indexes)

--- mongo/message-number-by-day.js
+++
@@ -1,9 +0,0 @@
-db[ 'history-room' ].aggregate([
-  { $match: { event: 'room:message' } },
-  {
-    $group: {
-      _id: { month: { $month: '$time' }, day: { $dayOfMonth: '$time' } },
-      count: { $sum: 1 }
-    }
-  }
-])
