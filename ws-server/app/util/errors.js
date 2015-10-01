d19a0b67a8e41bf0a523071353c553ef74c83541
Sebastien Pariaud <sebastienpariaud@gmail.com>
2015-10-01 11:53:32 +0200

    userVoiceHandler userBanHandler
    #624

--- ws-server/app/util/errors.js
+++ ws-server/app/util/errors.js
@@ -32,6 +32,7 @@ var errors = {
   'no-banned': 403,               // User is not banned from the room
   'banned': 403,                  // User is banned from the room or the 1&1
   'devoiced': 403,                // User is devoiced in the room
+  'voiced': 403,                  // User is voiced in the room
   'allowed': 403,                 // User is allowed in the room
   'owner': 403,                   // User is owner in the room
   'allow-pending': 403,           // User is in allowed pending list (private room)
