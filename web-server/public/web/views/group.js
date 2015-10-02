99693e7498f273a185e9b9ab81a9ec6d5da2cd06
Yann <y.lastapis@gmail.com>
2015-10-02 18:03:09 +0200

    #643 PENDING

--- web-server/public/web/views/group.js
+++ web-server/public/web/views/group.js
@@ -1,6 +1,6 @@
+var _ = require('underscore');
 var Backbone = require('backbone');
 var common = require('@dbrugne/donut-common/browser');
-var app = require('../models/app');
 
 var HomeView = Backbone.View.extend({
   tagName: 'div',
@@ -13,9 +13,23 @@ var HomeView = Backbone.View.extend({
   },
   render: function () {
     var group = this.model.toJSON();
+    var op = [];
+    var members = [];
+
+    _.each(group.members, function (u) { // prepare avatars
+      if (u.is_owner || u.is_op) {
+        u.avatar = common.cloudinary.prepare(u.avatar, 60);
+        op.push(u);
+      } else {
+        u.avatar = common.cloudinary.prepare(u.avatar, 34);
+        members.push(u);
+      }
+    });
     group.avatarUrl = common.cloudinary.prepare(group.avatar, 80);
-    var html = require('../templates/group.html')({group: group});
+    var html = require('../templates/group.html')({group: group, op: op, members: members});
     this.$el.html(html);
+
+    this.initializeTooltips();
     return this;
   },
   onFocusChange: function () {
@@ -24,6 +38,20 @@ var HomeView = Backbone.View.extend({
     } else {
       this.$el.hide();
     }
+  },
+  initializeTooltips: function () {
+    this.$('[data-toggle="tooltip"][data-type="room-mode"]').tooltip({
+      container: 'body'
+    });
+    this.$('[data-toggle="tooltip"][data-type="room-users"]').tooltip({
+      html: true,
+      animation: false,
+      container: 'body',
+      template: '<div class="tooltip tooltip-home-users" role="tooltip"><div class="tooltip-inner left" style="margin-top:3px;"></div></div>',
+      title: function () {
+        return '<div class="username" style="' + this.dataset.bgcolor + '">@' + this.dataset.username + '</div>';
+      }
+    });
   }
 
 });
