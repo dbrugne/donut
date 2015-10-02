6953f213d80dc869ae92b00ba7c92cf5aa6bfd08
Yoann Fuks <yoannfuks@hotmail.fr>
2015-10-02 11:02:32 +0200

    error fix
    #636

--- web-server/public/web/views/drawer-room-access.js
+++ web-server/public/web/views/drawer-room-access.js
@@ -209,7 +209,7 @@ var RoomAccessView = Backbone.View.extend({
 
     client.roomUpdate(this.roomId, {password: this.getPassword()}, _.bind(function (data) {
       if (data.err) {
-        return this.editError(data);
+        return this.setError(i18next.t('chat.form.errors.' + data.err));
       }
       this.trigger('close');
     }, this));
@@ -223,13 +223,6 @@ var RoomAccessView = Backbone.View.extend({
     }
 
     return null;
-  },
-  editError: function (dataErrors) {
-    var message = '';
-    _.each(dataErrors.err, function (error) {
-      message += i18next.t('chat.form.errors.' + error) + '<br>';
-    });
-    this.$errors.html(message).show();
   },
 
   initializeTooltips: function () {
