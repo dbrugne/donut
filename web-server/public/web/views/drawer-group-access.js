4289d5c9ad740b492a53aa0d27fcdb2b1a76a38f
Yann <y.lastapis@gmail.com>
2015-10-15 10:52:38 +0200

    #715

--- web-server/public/web/views/drawer-group-access.js
+++ web-server/public/web/views/drawer-group-access.js
@@ -80,6 +80,7 @@ var RoomAccessView = Backbone.View.extend({
     this.$checkboxGroupAllow = this.$('#input-allowgroupmember-checkbox');
     this.$password = this.$('.input-password');
     this.$randomPassword = this.$('.random-password');
+    this.$savePassword = this.$('.save-access');
     this.$countConditions = this.$('.counter');
     this.$conditions = this.$('#conditions-area');
 
@@ -170,6 +171,7 @@ var RoomAccessView = Backbone.View.extend({
     // Display block on click
     if (this.$toggleCheckbox.is(':checked')) {
       this.$password.removeAttr('disabled').removeClass('disabled');
+      this.$savePassword.removeAttr('disabled').removeClass('disabled');
       this.$randomPassword.removeClass('disabled');
       if (this.$password.val() === '' && !this.currentPassword) {
         this.$password.val(common.misc.randomString());
@@ -178,6 +180,7 @@ var RoomAccessView = Backbone.View.extend({
       }
     } else {
       this.$password.attr('disabled', true).addClass('disabled');
+      this.$savePassword.attr('disabled', true).addClass('disabled');
       this.$password.val('');
       this.$randomPassword.addClass('disabled');
     }
