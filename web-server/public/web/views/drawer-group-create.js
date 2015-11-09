15a1a62c497d9e816aca0a10d4a2854c0eec9b64
Yann <y.lastapis@gmail.com>
2015-11-09 12:05:23 +0100

    #849

--- web-server/public/web/views/drawer-group-create.js
+++ web-server/public/web/views/drawer-group-create.js
@@ -93,7 +93,7 @@ var DrawerGroupCreateView = Backbone.View.extend({
         return this.setError(i18next.t('global.unknownerror'));
       }
 
-      app.trigger('joinRoom', '#' + name + '/' + window.group_room_default);
+      app.trigger('joinGroup', {name: name, popin: true});
       this.reset();
       this.trigger('close');
     }, this));
