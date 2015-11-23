b97f4b97a45e4c26c3a6bd6b4cf5c7104943bdda
dbrugne <damien.brugne@gmail.com>
2015-11-23 18:46:05 +0100

    Minor

--- web-server/public/web/models/onetoone.js
+++ web-server/public/web/models/onetoone.js
@@ -103,6 +103,7 @@ var OneToOneModel = Backbone.Model.extend({
   onViewed: function (data) {
     if (this.get('unviewed') === true) {
       this.set('unviewed', false);
+      // @todo yls: why triggering a full redraw? We should just ask for a CSS class removing
       app.trigger('redrawNavigationOnes');
     }
     this.trigger('viewed', data);
