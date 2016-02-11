5d9af89c0b39e1a333e45fb2b85edc1757385725
Yann <y.lastapis@gmail.com>
2016-02-11 17:43:37 +0100

    #1268 new homepage

--- web-server/public/web/views/home-featured.js
+++ web-server/public/web/views/home-featured.js
@@ -1,3 +1,4 @@
+var $ = require('jquery');
 var _ = require('underscore');
 var Backbone = require('backbone');
 var common = require('@dbrugne/donut-common/browser');
@@ -6,7 +7,8 @@ var urls = require('../../../../shared/util/url');
 var HomeFeaturedView = Backbone.View.extend({
   template: require('../templates/home-featured.html'),
   events: {
-    // @todo implement navigation sliders
+    'click .action.left': 'onClickLeft',
+    'click .action.right': 'onClickRight'
   },
   render: function (list) {
     if (!_.has(list, 'rooms') || !_.has(list.rooms, 'list')) {
@@ -31,7 +33,7 @@ var HomeFeaturedView = Backbone.View.extend({
           card.join = urls(card, 'group', 'uri');
           card.url = urls(card, 'group', 'url');
           groups.push(card);
-        break;
+          break;
       }
     });
 
@@ -49,6 +51,28 @@ var HomeFeaturedView = Backbone.View.extend({
     this.$('[data-toggle="tooltip"]').tooltip({
       container: 'body'
     });
+  },
+  onClickLeft: function (event) {
+    var elt = $(event.currentTarget);
+    var container = elt.parents('.cards-container');
+    var first = container.find('.card-small:first');
+
+    first.animate({
+      marginLeft: '-=315px'
+    }, 300, function () {
+      first.appendTo(container);
+      first.css('margin-left', 0);
+    });
+  },
+  // click left
+  onClickRight: function (event) {
+    var elt = $(event.currentTarget);
+    var container = elt.parents('.cards-container');
+    var last = container.find('.card-small:last');
+
+    last.css('margin-left', -315);
+    last.prependTo(container);
+    last.animate({marginLeft: '0'}, 300);
   },
   _remove: function () {
     this.remove();
