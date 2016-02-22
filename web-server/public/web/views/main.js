2379ffb8c37319aa444e6ce49e313f29ffeabcb6
Yann <y.lastapis@gmail.com>
2016-02-22 18:07:50 +0100

    #1335 single rooms

--- web-server/public/web/views/main.js
+++ web-server/public/web/views/main.js
@@ -70,6 +70,7 @@ var MainView = Backbone.View.extend({
     'click .quit-group': 'onQuitGroup',
     'click .close-group': 'onCloseGroup',
     'click .close-discussion': 'onCloseDiscussion',
+    'click .leave-blocked': 'onLeaveBlockedDiscussion',
     'click .open-room-access': 'openRoomAccess',
     'change .language-switcher': 'switchLanguage',
 
@@ -484,6 +485,24 @@ var MainView = Backbone.View.extend({
                    // from interface
 
     return false; // stop propagation
+  },
+  onLeaveBlockedDiscussion: function(event) {
+    var $target = $(event.currentTarget);
+    if (!$target) {
+      return;
+    }
+
+    var roomId = $(event.currentTarget).data('room-id');
+    if (!roomId) {
+      return;
+    }
+
+    var room = app.rooms.get(roomId);
+    if (!room) {
+      return;
+    }
+
+    room.leaveBlocked();
   },
   onCloseGroup: function (event) {
     var $target = $(event.currentTarget);
